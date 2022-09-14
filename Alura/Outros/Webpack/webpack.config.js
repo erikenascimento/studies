4caad88f71702c8847d0a724ad766fab125d41fd
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-14 14:23:23 -0300

    Importando o path para atribuir um caminho... multisistema para o destino do webpack.config.js

--- Alura/Outros/Webpack/webpack.config.js
+++ Alura/Outros/Webpack/webpack.config.js
@@ -1,9 +1,11 @@
 const { Module } = require("webpack");
+const path = require("path");
 
 Module.exports = {
   entry: "./app/src/js/app.js", //delimita o arquivo principal que delimitará um caminho de expotação para todas as dependencias
-  output: { //gera um unico bandle com todas os scripts dependentes
+  output: {
+    //gera um unico bandle com todas os scripts dependentes
     filename: "bundle.js", //nome do arquivo do bundle
-    path: "./app/dist", //diretório do bundle
+    path: path.resolve(__dirname, "app/dist"), //diretório do bundle
   },
 };
