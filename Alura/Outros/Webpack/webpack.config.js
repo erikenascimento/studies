a90b7c7a7d8d8c709277a5cb4e5e36d1f8175a38
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-14 21:42:46 -0300

    Adicionado o html-webpack-plugin aos plugins

--- Alura/Outros/Webpack/webpack.config.js
+++ Alura/Outros/Webpack/webpack.config.js
@@ -1,9 +1,18 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './app/src/js/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'app/dist')
-    }
+  entry: "./app/src/js/app.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "app/dist"),
+    clean: true, //limpa a pasta especificada dos arquivos da build antiga
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./app/src/app.html",
+      filename: "app.html",
+      hash: true, //utilizado para identificar a bundle guardada em cache com uma hash
+    }),
+  ],
 };
