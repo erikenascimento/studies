78c2cbeac3d5c4c506b8d0949dea71ea16259b13
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-15 13:32:13 -0300

    Instalado o plugin copywebpack

--- Alura/Outros/Webpack/webpack.config.js
+++ Alura/Outros/Webpack/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./app/src/js/app.js",
+  entry: "./app/src/js/app.js", //localização do arquivo a ser copiado
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "app/dist"),
+    filename: "bundle.js", //nome do arquivo js que listará as dependencias
+    path: path.resolve(__dirname, "app/dist"), //caminho de destino da copia (dinâmico)
     clean: true, //limpa a pasta especificada dos arquivos da build antiga
   },
   plugins: [
@@ -13,6 +14,9 @@ module.exports = {
       template: "./app/src/app.html",
       filename: "app.html",
       hash: true, //utilizado para identificar a bundle guardada em cache com uma hash
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./app/src/css", to: "css" }], //to se refere ao nome do diretório criado apartir da origem from
     }),
   ],
 };
