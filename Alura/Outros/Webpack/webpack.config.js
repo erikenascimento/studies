6bd266ed5c57386fcb9b52eb0fd69be73c6057a7
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-14 21:01:11 -0300

    Substituindo o diretório Webpack pela versão da...  plataforma, já que deu algum erro no webpack.

--- Alura/Outros/Webpack/webpack.config.js
+++ Alura/Outros/Webpack/webpack.config.js
@@ -1,11 +1,9 @@
-const { Module } = require("webpack");
-const path = require("path");
+const path = require('path');
 
-Module.exports = {
-  entry: "./app/src/js/app.js", //delimita o arquivo principal que delimitará um caminho de expotação para todas as dependencias
-  output: {
-    //gera um unico bandle com todas os scripts dependentes
-    filename: "bundle.js", //nome do arquivo do bundle
-    path: path.resolve(__dirname, "app/dist"), //diretório do bundle
-  },
+module.exports = {
+    entry: './app/src/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'app/dist')
+    }
 };
