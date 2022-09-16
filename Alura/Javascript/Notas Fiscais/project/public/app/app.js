1fecb56c53ed09efb582d99608332d3a8d9ec5fc
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-16 18:56:59 -0300

    Limpando o código colocando parte da promise... como uma const para melhorar a legibilidade do código e faze-lo rodar recebendo um valor no seu parâmetro.

--- Alura/Javascript/Notas Fiscais/project/public/app/app.js
+++ Alura/Javascript/Notas Fiscais/project/public/app/app.js
@@ -1,14 +1,15 @@
 import { handleStatus, log } from "./utils/promise-helpers.js";
 import "./utils/array-helpers.js";
 
+const sumItems = (code) => (notas) =>
+  notas
+    .$flatMap((nota) => nota.itens)
+    .filter((item) => item.codigo == code)
+    .reduce((total, item) => total + item.valor, 0);
+
 document.querySelector("#myButton").onclick = () =>
   fetch("http://localhost:3000/notas")
     .then(handleStatus) //const importada auxiliará no tratamento dos status de resposta fetch
-    .then((notas) =>
-      notas
-        .$flatMap((nota) => nota.itens)
-        .filter((item) => item.codigo == "2143")
-        .reduce((total, item) => total + item.valor, 0)
-    )
+    .then(sumItems("2143 "))
     .then(console.log)
     .catch(console.log);
