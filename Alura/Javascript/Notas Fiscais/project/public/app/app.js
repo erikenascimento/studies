2ca837c9cadb6e63911913e859f263587881a421
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-16 18:50:26 -0300

    Criando o array-helper para auxiliar... no flatpacking do array buscado no fetch

--- Alura/Javascript/Notas Fiscais/project/public/app/app.js
+++ Alura/Javascript/Notas Fiscais/project/public/app/app.js
@@ -1,14 +1,14 @@
 import { handleStatus, log } from "./utils/promise-helpers.js";
+import "./utils/array-helpers.js";
 
 document.querySelector("#myButton").onclick = () =>
   fetch("http://localhost:3000/notas")
     .then(handleStatus) //const importada auxiliará no tratamento dos status de resposta fetch
     .then((notas) =>
-      notas.reduce((array, nota) => array.concat(nota.itens), [])
+      notas
+        .$flatMap((nota) => nota.itens)
+        .filter((item) => item.codigo == "2143")
+        .reduce((total, item) => total + item.valor, 0)
     )
-    .then(log)
-    .then((itens) => itens.filter((item) => item.codigo == "2143"))
-    .then(log)
-    .then((itens) => itens.reduce((total, item) => total + item.valor, 0))
     .then(console.log)
     .catch(console.log);
