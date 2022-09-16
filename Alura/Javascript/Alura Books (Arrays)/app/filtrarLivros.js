718564d6431036bbbe9d859a8268d8dbe3392a93
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-16 14:34:48 -0300

    Enxugando um pouco mais o codigo... com condicionais comparativas como funções no escopo global.

--- Alura/Javascript/Alura Books (Arrays)/app/filtrarLivros.js
+++ Alura/Javascript/Alura Books (Arrays)/app/filtrarLivros.js
@@ -7,19 +7,27 @@ function filtrarLivros() {
   //livrosFiltrados checará se a categoria do livro é igual a 'disponivel' se for, filtrará os livros por sua disponibilidade, caso não seja, filtrará pela categoria adequada ao botão clicado
   let livrosFiltrados =
     categoria == "disponivel"
-      ? livros.filter((livro) => livro.quantidade > 0)
-      : livros.filter((livro) => livro.categoria == categoria);
+      ? filtrarPorDisponibilidade()
+      : filtrarPorCategoria(categoria);
   exibirOsLivrosNaTela(livrosFiltrados);
-  if (categoria == 'disponivel') {
-    exibirValorTotalDosLivrosDisponiveisNaTela()
+  if (categoria == "disponivel") {
+    exibirValorTotalDosLivrosDisponiveisNaTela();
   }
 }
 
+function filtrarPorCategoria(categoria) {
+  return livros.filter((livro) => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+  return livros.filter((livro) => livro.quantidade > 0);
+}
+
 function exibirValorTotalDosLivrosDisponiveisNaTela() {
-    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
     <section id="valor_total_livros_disponiveis">
     <div class="livros__disponiveis">
       <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
     </div>
-    `
+    `;
 }
