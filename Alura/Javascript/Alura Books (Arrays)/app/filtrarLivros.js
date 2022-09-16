3c200afc04d2361e5cf1f51d9c12489241508489
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-16 13:45:13 -0300

    Modificado o script de filtro para também...  filtrar os livros pela disponibilidade através da propriedade value  no index.html

--- Alura/Javascript/Alura Books (Arrays)/app/filtrarLivros.js
+++ Alura/Javascript/Alura Books (Arrays)/app/filtrarLivros.js
@@ -4,6 +4,10 @@ botoes.forEach((btn) => btn.addEventListener("click", filtrarLivros));
 function filtrarLivros() {
   const elementoBtn = document.getElementById(this.id);
   const categoria = elementoBtn.value;
-  let livrosFiltrados = livros.filter((livro) => livro.categoria == categoria);
+  //livrosFiltrados checará se a categoria do livro é igual a 'disponivel' se for, filtrará os livros por sua disponibilidade, caso não seja, filtrará pela categoria adequada ao botão clicado
+  let livrosFiltrados =
+    categoria == "disponivel"
+      ? livros.filter((livro) => livro.quantidade > 0)
+      : livros.filter((livro) => livro.categoria == categoria);
   exibirOsLivrosNaTela(livrosFiltrados);
 }
