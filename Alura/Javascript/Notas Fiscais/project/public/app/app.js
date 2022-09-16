9facb45cb099aa81f3d82f39deb63744610130fb
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-16 18:27:21 -0300

    faz o fetch retornar uma array de itens...  e não uma array de arrays como anteriormente

--- Alura/Javascript/Notas Fiscais/project/public/app/app.js
+++ Alura/Javascript/Notas Fiscais/project/public/app/app.js
@@ -2,6 +2,15 @@ import { handleStatus } from "./utils/promise-helpers.js";
 
 document.querySelector("#myButton").onclick = () =>
   fetch("http://localhost:3000/notas")
-    .then(handleStatus) //const importada auxiliará no tratamento dos status de resposta fetch 
-    .then((notas) => console.log(notas))
+    .then(handleStatus) //const importada auxiliará no tratamento dos status de resposta fetch
+    .then((notas) =>
+      notas.reduce((array, nota) => array.concat(nota.itens), [])
+    )
+    .then((itens) => {
+      console.log(itens);
+      return itens;
+    })
+    .then((itens) => itens.filter((item) => item.codigo == "2143"))
+    .then((itens) => itens.reduce((total, item) => total + item.valor, 0))
+    .then(console.log)
     .catch(console.log);
