6f1f2d92052565c6b16e49f7b41fc18548113a56
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-17 13:02:52 -0300

    criado timeoutPromise para poder utilizar... timeouts em promisses

--- Alura/Javascript/Notas Fiscais/project/public/app/utils/promise-helpers.js
+++ Alura/Javascript/Notas Fiscais/project/public/app/utils/promise-helpers.js
@@ -1,7 +1,17 @@
-export const handleStatus = (res) =>
-  res.ok ? res.json() : Promise.reject(res.statusText);
+export const handleStatus = res =>
+	res.ok ? res.json() : Promise.reject(res.statusText);
 
-export const log = (param) => {
-  console.log(param);
-  return param;
+export const log = param => {
+	console.log(param);
+	return param;
+};
+
+export const timeoutPromise = (miliseconds, promise) => {
+	const timeout = new Promise((resolve, reject) =>
+		setTimeout(
+			() => reject(`Limite da promise excedido (limite: ${miliseconds} ms)`),
+			miliseconds
+		)
+	);
+	return Promise.race([timeout, promise]);
 };
