30bc8739c3eb3dd61e7ce1436cfe7b37aecdac43
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-17 11:29:59 -0300

    Criado operator takeUntil que vai limitar a... quantidade que um operação pode ser chamada

--- Alura/Javascript/Notas Fiscais/project/public/app/utils/operators.js
+++ Alura/Javascript/Notas Fiscais/project/public/app/utils/operators.js
@@ -10,3 +10,8 @@ export const compose =
 	valorInicial =>
 		fns.reduceRight((previousValue, fn) => fn(previousValue), valorInicial);
 //usando reduceRight para que a composição de funções funcione vindo da direita para a esquerda
+
+export const takeUntil = (times, fn) => () => times-- > 0 && fn();
+// sintaxe acima é um truqe para simular a sintaxe abaixo
+//if (times-- > 0) fn();
+//dessa forma será checado se times-- é maior que zero, caso seja, também checará a próxima condição que é uma chamada de função, isso é feito já que uma arrow function sem escopo só aceita um parametro
