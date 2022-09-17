3cd099d0b59e208a6e85a9a15222b4c89ae5efbd
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-17 16:43:11 -0300

    Feita alterações para utilizar corretamente a... o monádico Maybe caso a requisição da API retorne dados tipo null

--- Alura/Javascript/Notas Fiscais/project/public/app/nota/service.js
+++ Alura/Javascript/Notas Fiscais/project/public/app/nota/service.js
@@ -1,18 +1,21 @@
 import { handleStatus } from "../utils/promise-helpers.js";
 import { partialize, pipe } from "../utils/operators.js";
+import { Maybe } from "../utils/maybe.js";
 
 const API = "http://localhost:3000/notas"; //variável encapsulada pelo ES6
 
-const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
-const filterItemsByCode = (code, items) =>
-	items.filter(item => item.codigo == code);
-const sumItemsValue = items =>
-	items.reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notasM =>
+	notasM.map(notas => notas.$flatMap(nota => nota.itens));
+const filterItemsByCode = (code, itemsM) =>
+	itemsM.map(items => items.filter(item => item.codigo == code));
+const sumItemsValue = itemsM =>
+	itemsM.map(items => items.reduce((total, item) => total + item.valor, 0));
 
 export const notasService = {
 	listAll() {
 		return fetch(API)
 			.then(handleStatus)
+			.then(notas => Maybe.of(notas))
 			.catch(erro => {
 				console.log(erro);
 				return Promise.reject("Não foi possível obter as notas fiscais");
@@ -23,6 +26,8 @@ export const notasService = {
 		const filterItems = partialize(filterItemsByCode, code);
 		const sumItems = pipe(getItemsFromNotas, filterItems, sumItemsValue);
 
-		return this.listAll().then(sumItems);
+		return this.listAll()
+			.then(sumItems)
+			.then(result => result.getOrElse(0));
 	},
 };
