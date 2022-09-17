0941d5955ae68ae81a55702e3ac5b97bce5d1f33
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-17 09:47:17 -0300

    criado o util partialize em operators para...  auxiliar na criação de aplicações parciais como na filterItemsByCode na  função sumItems em service.js

--- Alura/Javascript/Notas Fiscais/project/public/app/nota/service.js
+++ Alura/Javascript/Notas Fiscais/project/public/app/nota/service.js
@@ -1,20 +1,21 @@
 import { handleStatus } from "../utils/promise-helpers.js";
+import { partialize } from "../utils/operators";
 
 const API = "http://localhost:3000/notas"; //variável encapsulada pelo ES6
 
 const getItemsFromNotas = (notas) => notas.$flatMap((nota) => nota.itens);
-const filterItemsByCode = code => items =>
+const filterItemsByCode = (code) => (items) =>
   items.filter((item) => item.codigo == code);
-  const sumItemsValue = (items) =>
+const sumItemsValue = (items) =>
   items.reduce((total, item) => total + item.valor, 0);
-  
-  export const notasService = {
-    listAll() {
-      return fetch(API).then(handleStatus);
-    },
-    
-    sumItems(code) {
-    const filterItems = filterItemsByCode.bind(null, code)
+
+export const notasService = {
+  listAll() {
+    return fetch(API).then(handleStatus);
+  },
+
+  sumItems(code) {
+    const filterItems = partialize(filterItemsByCode);
     return this.listAll().then(sumItems(code));
   },
 };
