8192423910182e2efeaffe246757fe9ad3b06620
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-17 09:43:16 -0300

    Refatorando o codigo p/ melhorar a legibilidade...  transformando o trecho de código que utiliza o flatmap, filter e reduce  em variáveis com nome mais léxicos

--- Alura/Javascript/Notas Fiscais/project/public/app/nota/service.js
+++ Alura/Javascript/Notas Fiscais/project/public/app/nota/service.js
@@ -2,18 +2,19 @@ import { handleStatus } from "../utils/promise-helpers.js";
 
 const API = "http://localhost:3000/notas"; //variável encapsulada pelo ES6
 
-const sumItems = (code) => (notas) =>
-  notas
-    .$flatMap((nota) => nota.itens)
-    .filter((item) => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0);
-
-export const notasService = {
-  listAll() {
-    return fetch(API).then(handleStatus);
-  },
-
-  sumItems(code) {
+const getItemsFromNotas = (notas) => notas.$flatMap((nota) => nota.itens);
+const filterItemsByCode = code => items =>
+  items.filter((item) => item.codigo == code);
+  const sumItemsValue = (items) =>
+  items.reduce((total, item) => total + item.valor, 0);
+  
+  export const notasService = {
+    listAll() {
+      return fetch(API).then(handleStatus);
+    },
+    
+    sumItems(code) {
+    const filterItems = filterItemsByCode.bind(null, code)
     return this.listAll().then(sumItems(code));
   },
 };
