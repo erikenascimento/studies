b704c614273e566abee26be34fcbea9611597b1c
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-17 10:56:37 -0300

    criando um compose em operator para poder... trabalhar com funcoes 'point-free' em service.js

--- Alura/Javascript/Notas Fiscais/project/public/app/utils/operators.js
+++ Alura/Javascript/Notas Fiscais/project/public/app/utils/operators.js
@@ -1,1 +1,7 @@
 export const partialize = (fn, ...args) => fn.bind(null, ...args);
+
+export const compose =
+	(...fns) =>
+	valorInicial =>
+		fns.reduceRight((previousValue, fn) => fn(previousValue), valorInicial);
+//usando reduceRight para que a composição de funções funcione vindo da direita para a esquerda
