00f94e086375cdd283710fa23c05ed8a187c8fa4
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-17 10:27:45 -0300

    Correção de bug, adicionando catch e...  aplicando a refatoração do tratamento dos dados da API.

--- Alura/Javascript/Notas Fiscais/project/public/app/nota/service.js
+++ Alura/Javascript/Notas Fiscais/project/public/app/nota/service.js
@@ -1,21 +1,29 @@
 import { handleStatus } from "../utils/promise-helpers.js";
-import { partialize } from "../utils/operators";
+import { partialize } from "../utils/operators.js";
 
 const API = "http://localhost:3000/notas"; //variável encapsulada pelo ES6
 
-const getItemsFromNotas = (notas) => notas.$flatMap((nota) => nota.itens);
-const filterItemsByCode = (code) => (items) =>
-  items.filter((item) => item.codigo == code);
-const sumItemsValue = (items) =>
-  items.reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const filterItemsByCode = (code, items) =>
+	items.filter(item => item.codigo == code);
+const sumItemsValue = items =>
+	items.reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
-  listAll() {
-    return fetch(API).then(handleStatus);
-  },
+	listAll() {
+		return fetch(API)
+			.then(handleStatus)
+			.catch(erro => {
+				console.log(erro);
+				return Promise.reject("Não foi possível obter as notas fiscais");
+			});
+	},
 
-  sumItems(code) {
-    const filterItems = partialize(filterItemsByCode);
-    return this.listAll().then(sumItems(code));
-  },
+	sumItems(code) {
+		const filterItems = partialize(filterItemsByCode, code);
+
+		return this.listAll().then(notas =>
+			sumItemsValue(filterItems(getItemsFromNotas(notas)))
+		);
+	},
 };
