3c8a19a3d72129bb1af32aedd2a2aba0b70c4339
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-18 18:15:08 -0300

    DrumKit project is finished

--- Projects/Javascript30/Drum Kit/scriptDrumKit.js
+++ Projects/Javascript30/Drum Kit/scriptDrumKit.js
@@ -1,8 +1,18 @@
 //It will listen to the keydown event and then will call a function
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //using ES6
-    const key = document.querySelector(`key[data-key="${e.keyCode}"]`); //will do the same but for the key effect
-    if(!audio) return; //stop the function when no audio was found for that key.
-    audio.currentTime = 0; // will enable the audio to be spammed
-    audio.play(); //if there's an audio, this will play it
+window.addEventListener("keydown", event => {
+	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+	if (!audio) return; //stop the function when no audio was found for that key.
+	audio.currentTime = 0; // will enable the audio to be spammed
+	audio.play();
+	key.classList.add("playing");
 });
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+function removeTransition(event) {
+	if (event.propertyName !== "transform") return;
+	this.classList.remove("playing");
+}
