d07f5ffc61690eff4a8ee0ec7e015933a851bedd
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-18 12:02:41 -0300

    Alterando o código para usar fetch.

--- Alura/Javascript/Doguito (CRUD)/admin/service/cliente-service.js
+++ Alura/Javascript/Doguito (CRUD)/admin/service/cliente-service.js
@@ -17,17 +17,9 @@ const criaNovaLinha = (nome, email) => {
 const tabela = document.querySelector("[data-tabela]");
 
 const listaClientes = () => {
-	const promise = new Promise((resolve, reject) => {
-		const http = new XMLHttpRequest();
-
-		http.open("GET", "http://localhost:3000/profile");
-		http.onload = () => {
-			if (http.status >= 400) reject(JSON.parse(http.response));
-			resolve(JSON.parse(http.response));
-		};
-		http.send();
-	});
-	return promise;
+	return fetch("http://localhost:3000/profile").then(resposta =>
+		resposta.json()
+	);
 };
 
 listaClientes().then(data =>
