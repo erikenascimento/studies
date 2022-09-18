e466a00b1b6943f9c4a33ae93987ca6bdec48fa5
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-17 21:49:50 -0300

    Fazendo a requisição da API através de promises

--- Alura/Javascript/Doguito (CRUD)/admin/service/cliente-service.js
+++ Alura/Javascript/Doguito (CRUD)/admin/service/cliente-service.js
@@ -16,13 +16,22 @@ const criaNovaLinha = (nome, email) => {
 
 const tabela = document.querySelector("[data-tabela]");
 
-const http = new XMLHttpRequest();
+const listaClientes = () => {
+	const promise = new Promise((resolve, reject) => {
+		const http = new XMLHttpRequest();
 
-http.open("GET", "http://localhost:3000/profile");
-http.send();
-http.onload = () => {
-	const data = JSON.parse(http.response);
-	data.forEach(elemento => {
-		tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
+		http.open("GET", "http://localhost:3000/profile");
+		http.onload = () => {
+			if (http.status >= 400) reject(JSON.parse(http.response));
+			resolve(JSON.parse(http.response));
+		};
+		http.send();
 	});
+	return promise;
 };
+
+listaClientes().then(data =>
+	data.forEach(elemento =>
+		tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+	)
+);
