01e3dd57c8d5182c672af446b556d1364f80ff08
erikenascimento <nascimento.lf.erike@gmail.com>
2022-09-24 10:22:42 -0300

    Arquivo final do primeiro dia do 7DC... Lógica JS

---
+++ Projects/7 Days of Code/Logica JS/OperacoesBooleanas.js
@@ -0,0 +1,26 @@
+let numeroUm = 1;
+let stringUm = "1";
+let numeroTrinta = 30;
+let stringTrinta = "30";
+let numeroDez = 10;
+let stringDez = "10";
+
+numeroUm == stringUm
+	? console.log(
+			"As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes"
+	  )
+	: console.log("As variáveis numeroUm e stringUm não tem o mesmo valor");
+
+numeroTrinta === stringTrinta
+	? console.log(
+			"As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo"
+	  )
+	: console.log(
+			"As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo"
+	  );
+
+numeroDez == stringDez
+	? console.log(
+			"As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes"
+	  )
+	: console.log("As variáveis numeroDez e stringDez não tem o mesmo valor");
