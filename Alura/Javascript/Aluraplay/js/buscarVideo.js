e5821bd81e9cd7063bb3e5d3840a6088b10c3fc3
erikenascimento <nascimento.lf.erike@gmail.com>
2022-12-29 14:02:18 -0300

    agora o script renderiza o item pesquisado... na página ao invés de mostrar um objeto no console

--- Alura/Javascript/Aluraplay/js/buscarVideo.js
+++ Alura/Javascript/Aluraplay/js/buscarVideo.js
@@ -1,4 +1,5 @@
 import { conectaApi } from "./conectaApi.js";
+import constroiCard from "./mostrarVideos.js";
 
 async function buscarVideo(evento) {
 	evento.preventDefault();
@@ -6,7 +7,23 @@ async function buscarVideo(evento) {
 	const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
 	const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
-	console.log(busca);
+	const lista = document.querySelector("[data-lista]");
+
+	//sempre remove um filho de uma lista enquanto houverem filhos nesta lista
+	while (lista.firstChild) {
+		lista.removeChild(lista.firstChild);
+	}
+
+	busca.forEach(elemento =>
+		lista.appendChild(
+			constroiCard(
+				elemento.titulo,
+				elemento.descricao,
+				elemento.url,
+				elemento.imagem
+			)
+		)
+	);
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
