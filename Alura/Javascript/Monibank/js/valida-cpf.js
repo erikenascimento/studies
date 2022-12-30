e0d62c771eecbf5d8801be64c861f4131430f322
erikenascimento <nascimento.lf.erike@gmail.com>
2022-12-30 13:07:35 -0300

    adicionado script para validar primeiro... numero verificador do cpf

--- Alura/Javascript/Monibank/js/valida-cpf.js
+++ Alura/Javascript/Monibank/js/valida-cpf.js
@@ -1,6 +1,8 @@
 export default function ehUmCPF(campo) {
 	const cpf = campo.value.replace(/\.|-/g, "");
+
 	validaNumerosRepetidos(cpf);
+	validaPrimeiroDigito(cpf);
 
 	console.log(validaNumerosRepetidos(cpf));
 }
@@ -20,4 +22,22 @@ function validaNumerosRepetidos(cpf) {
 	];
 
 	return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+	let somea = 0;
+	let multiplicador = 10;
+
+	for (let tamanho = 0; tamanho < 9; tamanho++) {
+		soma += cpf[tamanho] * multiplicador;
+		multiplicador__;
+	}
+
+	soma = (soma * 10) % 11;
+
+	if (soma == 10 || soma == 1) {
+		soma = 0;
+	}
+
+	return soma != cpf[9];
 }
