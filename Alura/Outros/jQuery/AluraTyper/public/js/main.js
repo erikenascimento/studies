0c578c75633ae074863fd8951cffc940a78a3572
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-13 14:12:22 -0300

    Implementando o contador de caracteres... e palavras no text area

--- Alura/Outros/jQuery/AluraTyper/public/js/main.js
+++ Alura/Outros/jQuery/AluraTyper/public/js/main.js
@@ -1,5 +1,18 @@
 let frase = $(".frase").text();
-let numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
+let tamanhoFrase = $("#tamanho-frase");
+
+let numPalavras = frase.split(/\s+/).length - 1;
 
 tamanhoFrase.text(numPalavras);
+
+let campoDigitacao = $("#campo-digitacao");
+let contadorCaracteres = $("#contador-caracteres");
+let contadorPalavras = $("#contador-palavras");
+
+campoDigitacao.on("input", function () {
+	let numCaracteres = campoDigitacao.val().length;
+	let numPalavras = campoDigitacao.val().split(/\s+/).length - 1;
+
+	contadorCaracteres.text(numCaracteres);
+	contadorPalavras.text(numPalavras);
+});
