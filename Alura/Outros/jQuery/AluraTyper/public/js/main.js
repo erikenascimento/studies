3ce5e166fc8ebb9a177e15bcf931e5303f938cd3
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-14 13:21:34 -0300

    Inserida função de adicionar dados num placar

--- Alura/Outros/jQuery/AluraTyper/public/js/main.js
+++ Alura/Outros/jQuery/AluraTyper/public/js/main.js
@@ -29,7 +29,7 @@ function InicializaContadores() {
 let tempoDigitacao = $("#tempo-digitacao").text();
 const tempoInicial = tempoDigitacao;
 
-function CampoAtivaDesativa() {
+function CampoAtivaDesativaBorda() {
 	campoDigitacao.toggleClass("campoAtivado");
 	campoDigitacao.toggleClass("campoDesativado");
 }
@@ -41,10 +41,8 @@ function InicializaCronometro() {
 			tempoDigitacao--;
 			$("#tempo-digitacao").text(tempoDigitacao);
 			if (tempoDigitacao <= 0) {
-				campoDigitacao.attr("disabled", true);
 				clearInterval(cronometroID);
-				$("#botao-reiniciar").attr("disabled", false);
-				CampoAtivaDesativa();
+				FinalizaJogo();
 			}
 		}, 1000);
 	});
@@ -53,10 +51,8 @@ function InicializaCronometro() {
 function InicializaMarcadores() {
 	campoDigitacao.on("input", function () {
 		let digitando = campoDigitacao.val();
-		console.log("digitando", digitando);
 		// Gambiarra pois o prettier está adicionando 5 espaços ao elemento
 		let comparavel = frase.substring(5, digitando.length + 5);
-		console.log("comparavel", comparavel);
 
 		if (digitando == comparavel) {
 			campoDigitacao.addClass("campoCorreto");
@@ -68,8 +64,37 @@ function InicializaMarcadores() {
 	});
 }
 
+function FinalizaJogo() {
+	campoDigitacao.attr("disabled", true);
+	$("#botao-reiniciar").attr("disabled", false);
+	CampoAtivaDesativaBorda();
+	InserePlacar();
+}
+
+function InserePlacar() {
+	let corpoTabela = $("#corpo-tabela");
+
+	let jogador = "Erik";
+
+	corpoTabela.append(`
+	<tr>
+		<td>${jogador}</td>
+		<td>${parseInt(contadorPalavras.text()) + 1}</td>
+		<td>
+			<a href="#">
+				<i class="small material-icons" id="botao-remover">delete</i>
+			</a>
+		<td>
+	</tr>`);
+
+	$("#botao-remover").click(function (event) {
+		event.preventDefault();
+		$(this).parent().parent().parent().remove();
+	});
+}
+
 function ReiniciaJogo() {
-	CampoAtivaDesativa();
+	CampoAtivaDesativaBorda();
 	campoDigitacao.removeClass("campoCorreto");
 	campoDigitacao.removeClass("campoIncorreto");
 	campoDigitacao.attr("disabled", false);
