ba9f69a3286bedc32711493712b477679fb3c83f
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-14 11:10:35 -0300

    Adicionando algumas manipulações de css com js

--- Alura/Outros/jQuery/AluraTyper/public/js/main.js
+++ Alura/Outros/jQuery/AluraTyper/public/js/main.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
 	InicializaContadores();
 	InicializaCronometro();
 	$("#botao-reiniciar").click(ReiniciaJogo);
+	InicializaMarcadores();
 });
 
 function InicializaContadores() {
@@ -28,20 +29,49 @@ function InicializaContadores() {
 let tempoDigitacao = $("#tempo-digitacao").text();
 const tempoInicial = tempoDigitacao;
 
+function CampoAtivaDesativa() {
+	campoDigitacao.toggleClass("campoAtivado");
+	campoDigitacao.toggleClass("campoDesativado");
+}
+
 function InicializaCronometro() {
 	campoDigitacao.one("focus", function () {
+		$("#botao-reiniciar").attr("disabled", true);
 		let cronometroID = setInterval(function () {
 			tempoDigitacao--;
 			$("#tempo-digitacao").text(tempoDigitacao);
 			if (tempoDigitacao <= 0) {
 				campoDigitacao.attr("disabled", true);
 				clearInterval(cronometroID);
+				$("#botao-reiniciar").attr("disabled", false);
+				CampoAtivaDesativa();
 			}
 		}, 1000);
 	});
 }
 
+function InicializaMarcadores() {
+	campoDigitacao.on("input", function () {
+		let digitando = campoDigitacao.val();
+		console.log("digitando", digitando);
+		// Gambiarra pois o prettier está adicionando 5 espaços ao elemento
+		let comparavel = frase.substring(5, digitando.length + 5);
+		console.log("comparavel", comparavel);
+
+		if (digitando == comparavel) {
+			campoDigitacao.addClass("campoCorreto");
+			campoDigitacao.removeClass("campoIncorreto");
+		} else {
+			campoDigitacao.addClass("campoIncorreto");
+			campoDigitacao.removeClass("campoCorreto");
+		}
+	});
+}
+
 function ReiniciaJogo() {
+	CampoAtivaDesativa();
+	campoDigitacao.removeClass("campoCorreto");
+	campoDigitacao.removeClass("campoIncorreto");
 	campoDigitacao.attr("disabled", false);
 	campoDigitacao.val("");
 	contadorPalavras.text("0");
