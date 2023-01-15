2d8a3a5c9a9101103721d27eb0531fd42008a925
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-15 19:04:13 -0300

    Implementado um buscador de frase pelo ID.

--- Alura/Outros/jQuery/AluraTyper/public/js/frase.js
+++ Alura/Outros/jQuery/AluraTyper/public/js/frase.js
@@ -1,14 +1,51 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+	$("#spinner").toggle();
+
+	$.get("http://localhost:3000/frases", trocaFraseAleatoria)
+		.fail(function () {
+			$("#erro").toggle();
+			setTimeout(function () {
+				$("#erro").toggle();
+			}, 3000);
+		})
+		.always(function () {
+			$("#spinner").toggle();
+		});
 }
 
 function trocaFraseAleatoria(data) {
-    var frase = $(".frase");
-    var numeroAleatorio = Math.floor(Math.random() * data.length);
+	var frase = $(".frase");
+	var numeroAleatorio = Math.floor(Math.random() * data.length);
 
-    frase.text(data[numeroAleatorio].texto);
-    atualizaTamanhoFrase();
-    atualizaTempoInicial(data[numeroAleatorio].tempo);
+	frase.text(data[numeroAleatorio].texto);
+	atualizaTamanhoFrase();
+	atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase() {
+	$("#spinner").toggle();
+
+	let fraseId = $("#frase-id").val();
+	let dados = { id: fraseId };
+
+	$.get("http://localhost:3000/frases", dados, trocaFrase)
+		.fail(function () {
+			$("#erro").toggle();
+			setTimeout(function () {
+				$("#erro").toggle();
+			}, 3000);
+		})
+		.always(function () {
+			$("#spinner").toggle();
+		});
+}
+
+function trocaFrase(data) {
+	$(".frase").text(data.texto);
+
+	atualizaTamanhoFrase();
+	atualizaTempoInicial(data.tempo);
 }
