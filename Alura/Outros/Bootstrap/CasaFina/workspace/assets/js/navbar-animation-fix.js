82f071c2af892b71b8647c9e2854971d95e50bae
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-16 23:05:01 -0300

    Arquivo final do curso bootstrap, casafina... Versao final do projeto já que o ultimo passo tinha um bug que... requeriria uma completa reestruturização do código.

--- Alura/Outros/Bootstrap/CasaFina/workspace/assets/js/navbar-animation-fix.js
+++
@@ -1,7 +0,0 @@
-$("#collapse-navbar").on("show.bs.collapse", function () {
-	$(".CasaFina-banner").css("transform", "translate(-50%, 10%");
-});
-
-$("#collapse-navbar").on("hide.bs.collapse", function () {
-	$(".CasaFina-banner").css("transform", "translate(-50%, -50%");
-});
