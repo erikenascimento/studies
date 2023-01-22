53974c4521b3cd5aaf6cf45b0e516da1b0b57a2f
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-21 22:39:16 -0300

    Fazendo a segunda parte de sprites do projeto... de conclusão javascript

--- Alura/Javascript/Jogo_da_Forca/gallows/js/sprite.js
+++ Alura/Javascript/Jogo_da_Forca/gallows/js/sprite.js
@@ -2,15 +2,28 @@ function createSprite(seletorCSS) {
 	var sprite = {
 		seletor: $(seletorCSS),
 		numeroFrame: 1,
+		mudaFrame: function (modificador) {
+			sprite.seletor.toggleClass(`frame${sprite.numeroFrame - modificador}`);
+		},
 		nextFrame: function () {
+			sprite.numeroFrame++;
 			if (sprite.numeroFrame <= 9) {
 				if (sprite.numeroFrame > 1) {
-					sprite.seletor.toggleClass(`frame${sprite.numeroFrame - 1}`); //remove classe anterior
+					sprite.mudaFrame(1);
 				}
-
-				sprite.seletor.toggleClass(`frame${sprite.numeroFrame}`); //adiciona classe atual
-				sprite.numeroFrame++;
+				sprite.mudaFrame(0);
 			}
+		},
+		isFinished: function () {
+			if (sprite.numeroFrame >= 9) {
+				return true;
+			} else {
+				return false;
+			}
+		},
+		reset: function () {
+			sprite.mudaFrame(0);
+			sprite.numeroFrame = 1;
 		},
 	};
 
