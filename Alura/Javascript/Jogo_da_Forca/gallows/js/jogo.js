f2165e12de8eb5d09c212a2838bb92a17b008487
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-22 12:15:33 -0300

    Criando o template da segunda parte do segundo... desafio do projeto de conclusão js

--- Alura/Javascript/Jogo_da_Forca/gallows/js/jogo.js
+++ Alura/Javascript/Jogo_da_Forca/gallows/js/jogo.js
@@ -1,7 +1,12 @@
-var criaJogo = function () {
+var criaJogo = function (sprite) {
 	var _etapa = 1;
 	var palavraSecreta = "";
 	var _lacunas = [];
+
+	var processaChute = function (chute) {
+		// preencher as lacunas se o chute estiver correto
+		// se errar, avançar o frame do sprite
+	};
 
 	var setPalavraSecreta = function (palavra) {
 		palavraSecreta = palavra;
@@ -22,5 +27,6 @@ var criaJogo = function () {
 		setPalavraSecreta: setPalavraSecreta,
 		getLacunas: getLacunas,
 		getEtapa: getEtapa,
+		processaChute: processaChute,
 	};
 };
