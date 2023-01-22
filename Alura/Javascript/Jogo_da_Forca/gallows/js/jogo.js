6efeb24b782b241a8d9c9f16c7ff2976de6f527a
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-21 22:41:42 -0300

    Criando esboço da próxima etapa do projeto.

---
+++ Alura/Javascript/Jogo_da_Forca/gallows/js/jogo.js
@@ -0,0 +1,13 @@
+var criaJogo = function () {
+	var setPalavraSecreta = function (palavra) {};
+
+	var getLacunas = function () {};
+
+	var getEtapa = function () {};
+
+	return {
+		setPalavraSecreta: setPalavraSecreta,
+		getLacunas: getLacunas,
+		getEtapa: getEtapa,
+	};
+};
