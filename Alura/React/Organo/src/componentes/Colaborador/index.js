700c00151e2c2724620d8905d65145345e796e75
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-25 19:17:32 -0300

    criando o componente de colaborador e... copiando e colando o estilo css fornecido pelo UX

---
+++ Alura/React/Organo/src/componentes/Colaborador/index.js
@@ -0,0 +1,20 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+	return (
+		<div className="colaborador">
+			<div className="cabecalho">
+				<img
+					src="https://github.com/erikenascimento.png"
+					alt="Erike Nascimento"
+				/>
+			</div>
+			<div className="rodape">
+				<h4>Erike Nascimento</h4>
+				<h5>Instrutor</h5>
+			</div>
+		</div>
+	);
+};
+
+export default Colaborador;
