11da34e348ebfc3e004be94a671408857512a552
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-25 15:30:25 -0300

    Modificando o componente para disponibilizarem... as cores adequadas para a estilização dos mesmos

--- Alura/React/Organo/src/App.js
+++ Alura/React/Organo/src/App.js
@@ -4,6 +4,44 @@ import Formulario from "./componentes/Formulario";
 import Time from "./componentes/Time";
 
 function App() {
+	const times = [
+		{
+			nome: "Programação",
+			corPrimaria: "#57c298",
+			corSecundaria: "#d9f7e9",
+		},
+		{
+			nome: "Front-End",
+			corPrimaria: "#82CFFA",
+			corSecundaria: "#E8F8FF",
+		},
+		{
+			nome: "Data Science",
+			corPrimaria: "#A6D157",
+			corSecundaria: "#F0F8E2",
+		},
+		{
+			nome: "Devops",
+			corPrimaria: "#E06B69",
+			corSecundaria: "#FDE7E8",
+		},
+		{
+			nome: "UX e Design",
+			corPrimaria: "#DB6EBF",
+			corSecundaria: "#FAE9F5",
+		},
+		{
+			nome: "Mobile",
+			corPrimaria: "#FFBA05",
+			corSecundaria: "#FFF5D9",
+		},
+		{
+			nome: "Inovação e Gestão",
+			corPrimaria: "#FF8A29",
+			corSecundaria: "#FFEEDF",
+		},
+	];
+
 	const [colaboradores, setColaboradores] = useState([]);
 
 	const aoNovoColaboradorAdicionado = colaborador => {
@@ -19,10 +57,14 @@ function App() {
 					aoNovoColaboradorAdicionado(colaborador)
 				}
 			/>
-			<Time nome="Programação" />
-			<Time nome="Front-End" />
-			<Time nome="Data Science" />
-			<Time nome="Devops" />
+			{times.map(time => (
+				<Time
+					key={time.nome}
+					nome={time.nome}
+					corPrimaria={time.corPrimaria}
+					corSecundaria={time.corSecundaria}
+				/>
+			))}
 		</div>
 	);
 }
