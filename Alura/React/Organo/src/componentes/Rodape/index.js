0c7fc043bed416751f0895c8bb8fa61e78a273f2
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-25 23:11:37 -0300

    Arquivo inicial do segundo curso React com JS

--- Alura/React/Organo/src/componentes/Rodape/index.js
+++ Alura/React/Organo/src/componentes/Rodape/index.js
@@ -1,17 +1,36 @@
-import "./Rodape.css";
+
+import './rodape.css'
 
 const Rodape = () => {
-	return (
-		<section className="rodapePagina">
-			<div>
-				<img src="/imagens/fb.png" alt="Logo do Facebook" />
-				<img src="/imagens/tw.png" alt="Logo do Twitter" />
-				<img src="/imagens/ig.png" alt="Logo do Instagram" />
-			</div>
-			<img src="/imagens/logo.png" alt="Logo do Organo" />
-			<p>Desenvolvido por Alura</p>
-		</section>
-	);
-};
+    return (<footer className="footer">
+       <section>
+       <ul>
+            <li>
+                <a href="facebook.com" target="_blank">
+                    <img src="/imagens/facebook.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="twitter.com" target="_blank">
+                    <img src="/imagens/twitter.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="instagram.com" target="_blank">
+                    <img src="/imagens/instagram.png" alt="" />
+                </a>
+            </li>
+        </ul>
+       </section>
+       <section>
+       <img src="/imagens/logo.png" alt="" />
+       </section>
+       <section>
+        <p>
+            Desenvolvido por Alura.
+        </p>
+       </section>
+    </footer>)
+}
 
-export default Rodape;
+export default Rodape
--- Alura/React/Organo/src/componentes/Time/index.js
+++ Alura/React/Organo/src/componentes/Time/index.js
@@ -1,28 +1,17 @@
-import Colaborador from "../Colaborador";
-import "./Time.css";
+import Colaborador from '../Colaborador'
+import './time.css'
 
-const Time = props => {
-	return (
-		props.colaboradores.length > 0 && (
-			<section
-				className="time"
-				style={{ backgroundColor: props.corSecundaria }}
-			>
-				<h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-				<div className="colaboradores">
-					{props.colaboradores.map(colaborador => (
-						<Colaborador
-							key={colaborador.nome}
-							corDeFundo={props.corPrimaria}
-							nome={colaborador.nome}
-							cargo={colaborador.cargo}
-							imagem={colaborador.imagem}
-						/>
-					))}
-				</div>
-			</section>
-		)
-	);
-};
+const Time = ({ time, colaboradores }) => {
+    return (
 
-export default Time;
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+            <div className='colaboradores'>
+                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
+            </div>
+        </section>
+
+    )
+}
+
+export default Time
