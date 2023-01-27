c9886c1c16823eae34fd732de6104a3cb2390d22
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-27 11:00:42 -0300

    Criando e importando estilizacao do cabecalho

--- Alura/React/alura_space/src/App.js
+++ Alura/React/alura_space/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PaginaInicial from "./Components/Pagina_Inicial";
+import "./styles/estilosGlobais.css";
 
 export default class App extends Component {
 	render() {
--- Alura/React/alura_space/src/Components/Cabecalho/index.jsx
+++ Alura/React/alura_space/src/Components/Cabecalho/index.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import logo from './logo.png'
 import search from './search.png'
+import './Cabecalho.module.css'
 
 export default function Cabecalho() {
     return (
-        <header>
+        <header className="cabecalho">
             <img src={logo} alt="Logo do Alura Space"/>
-            <div>
-                <input type="text" placeholder="O que você procura?"/>
+            <div className="cabecalho__container">
+                <input className="cabecalho__input" type="text" placeholder="O que você procura?"/>
                 <img src={search} alt="Ícone de busca (lupa)"/>
             </div>
         </header>
