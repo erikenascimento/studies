9118983a4aded4f1f72855a9895b1dbd31cd553d
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-27 11:53:20 -0300

    Banner criado e estilizado

--- Alura/React/alura_space/src/paginas/PaginaInicial/index.jsx
+++ Alura/React/alura_space/src/paginas/PaginaInicial/index.jsx
@@ -1,11 +1,22 @@
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
+import styles from './PaginaInicial.module.scss'
+import banner from './banner.png'
 
 export default function PaginaInicial(){
   return (
     <>
     <Cabecalho />
+    <main>
+      <section className={styles.principal}>
     <Menu />
+    <div className={styles.principal__imagem}>
+      <h1>A galeria mais completa do espaço</h1>
+      <img src={banner} alt="Terra vista do espaço"/>
+    </div>
+
+      </section>
+    </main>
     </>
   )
 }
