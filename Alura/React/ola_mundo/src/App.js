2bdf847ab2eb03b4deaf058f83460b9d96d88615
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-27 19:47:48 -0300

    Criando novo projeto react com as modificações... iniciais necessárias.

---
+++ Alura/React/ola_mundo/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+	render() {
+		return (
+			<div style={{ textAlign: "center" }}>
+				<h1>Ay Lmao</h1>
+				<img
+					src="https://media.tenor.com/_LOm-JCkWncAAAAi/alienpls.gif"
+					alt="I can't believe my fucking eyes"
+				/>
+			</div>
+		);
+	}
+}
