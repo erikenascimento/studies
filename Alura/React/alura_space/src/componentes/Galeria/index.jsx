ca46527ed859116d446fe893532b9fc69b0d21c7
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-27 15:32:58 -0300

    Finalizando o projeto adicionando... funcionalidade de filtrar imagens por tags

--- Alura/React/alura_space/src/componentes/Galeria/index.jsx
+++ Alura/React/alura_space/src/componentes/Galeria/index.jsx
@@ -3,13 +3,23 @@ import Tags from "../Tags";
 import styles from "./Galeria.module.scss";
 import fotos from "./fotos.json";
 import Cards from "./Cards";
+import { useState } from "react";
 
 export default function Galeria() {
+	const [itens, setItens] = useState(fotos);
+	const tags = [...new Set(fotos.map(valor => valor.tag))];
+
+	const filtraFotos = tag => {
+		const novasFotos = fotos.filter(foto => foto.tag === tag);
+
+		setItens(novasFotos);
+	};
+
 	return (
 		<section className={styles.galeria}>
 			<h2>Navegue pela galeria</h2>
-			<Tags />
-			<Cards fotos={fotos} styles={styles} />
+			<Tags tags={tags} filtraFotos={filtraFotos} setItens={setItens} />
+			<Cards fotos={itens} styles={styles} />
 		</section>
 	);
 }
