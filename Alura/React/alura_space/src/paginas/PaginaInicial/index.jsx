ef51629bc14fa5af32e81fe6a0703206b8c676e3
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-27 15:08:21 -0300

    Criando componente de fotos populares e... refatorando um pouco o css do projeto

--- Alura/React/alura_space/src/paginas/PaginaInicial/index.jsx
+++ Alura/React/alura_space/src/paginas/PaginaInicial/index.jsx
@@ -1,28 +1,29 @@
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
-import styles from './PaginaInicial.module.scss'
-import banner from './banner.png'
+import styles from "./PaginaInicial.module.scss";
+import banner from "./banner.png";
 import Rodape from "../../componentes/Rodape";
 import Galeria from "../../componentes/Galeria";
+import Populares from "../../componentes/Populares";
 
-export default function PaginaInicial(){
-  return (
-    <>
-    <Cabecalho />
-    <main>
-      <section className={styles.principal}>
-    <Menu />
-    <div className={styles.principal__imagem}>
-      <h1>A galeria mais completa do espaço</h1>
-      <img src={banner} alt="Terra vista do espaço"/>
-    </div>
-
-      </section>
-    </main>
-    <div>
-      <Galeria />
-    </div>
-    <Rodape />
-    </>
-  )
+export default function PaginaInicial() {
+	return (
+		<>
+			<Cabecalho />
+			<main>
+				<section className={styles.principal}>
+					<Menu />
+					<div className={styles.principal__imagem}>
+						<h1>A galeria mais completa do espaço</h1>
+						<img src={banner} alt="Terra vista do espaço" />
+					</div>
+				</section>
+				<div className={styles.galeria}>
+					<Galeria />
+					<Populares />
+				</div>
+			</main>
+			<Rodape />
+		</>
+	);
 }
