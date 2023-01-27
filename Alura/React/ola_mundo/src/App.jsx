c933d1b07a3b222b637b5e2ead61c704a5735687
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-27 20:16:21 -0300

    criando a rota de páginas com react-router-dom

---
+++ Alura/React/ola_mundo/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/SobreMim";
+
+export default function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Inicio />} />
+				<Route path="/sobremim" element={<SobreMim />} />
+				<Route path="*" element={<h1>Not Found</h1>} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
