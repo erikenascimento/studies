49a01ef1e6a2e6de60d2764490f3a08181408411
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-27 22:20:18 -0300

    Utilizando o NavLink do react-router-dom

--- Alura/React/ola_mundo/src/components/Menu/MenuLink/index.jsx
+++ Alura/React/ola_mundo/src/components/Menu/MenuLink/index.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./MenuLink.module.scss";
 
 export default function MenuLink({ children, to }) {
-	const localizacao = useLocation();
-
 	return (
-		<Link
+		<NavLink
+			className={({ isActive }) => `
+                ${styles.navegacao__link}
+                ${isActive ? styles.navegacao__linkDestacado : ""}
+            `}
 			to={to}
-			className={`${styles.navegacao__link} ${
-				localizacao.pathname === to ? styles.navegacao__linkDestacado : ""
-			}`}
+			end
 		>
 			{children}
-		</Link>
+		</NavLink>
 	);
 }
