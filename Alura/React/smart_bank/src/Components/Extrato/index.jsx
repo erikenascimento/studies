ff2fdcef1dabe0dfde6d51787f93ac172f602240
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-30 18:48:22 -0300

    Criando os componentes Itens e Item

--- Alura/React/smart_bank/src/Components/Extrato/index.jsx
+++ Alura/React/smart_bank/src/Components/Extrato/index.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Box, Botao } from "../UI";
 import { extratoLista } from "../../info.js";
+import Items from "../Items";
 
-export default function Extrato() {
+const Extrato = () => {
   return (
     <Box>
       {extratoLista.updates.map(({ id, type, from, value, date }) => {
         return (
-          <div key={id}>
-            <div>{type}</div>
-            <div>{from}</div>
-          </div>
+          <Items key={id} type={type} from={from} value={value} date={date} />
         );
       })}
       <Botao>Ver Mais</Botao>
     </Box>
   );
-}
+};
+
+export default Extrato;
---
+++ Alura/React/smart_bank/src/Components/Items/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import Item from "./Item";
+
+const Items = styled.div`
+  align-items: center;
+  box-shadow: 4px 4px 20px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  display: flex;
+  font-size: 12px;
+  justify-content space-around;
+  margin: 2px 0;  
+  padding: 10px;
+`;
+
+export default (props) => {
+  return (
+    <Items>
+      <Item {...props} />
+      <span>{props.date}</span>
+    </Items>
+  );
+};
