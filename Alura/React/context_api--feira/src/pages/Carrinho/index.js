7a22101629728eed32b2887616c6641d119b7c1a
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-01 13:25:36 -0300

    Criando e utilizando o contexto de pagamento... na pagina de carrinho

--- Alura/React/context_api--feira/src/pages/Carrinho/index.js
+++ Alura/React/context_api--feira/src/pages/Carrinho/index.js
@@ -1,8 +1,9 @@
 import { Button, Snackbar, InputLabel } from "@material-ui/core";
 import MuiAlert from "@material-ui/lab/Alert";
 import { useCarrinhoContext } from "common/context/Carrinho";
+import { PagamentoContext } from "common/context/Pagamento";
 import Produto from "components/Produto";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import {
 	Container,
@@ -14,10 +15,14 @@ import {
 function Carrinho() {
 	const [openSnackbar, setOpenSnackbar] = useState(false);
 	const { carrinho } = useCarrinhoContext();
+	const { formaPagamento } = useContext(PagamentoContext);
+
 	const history = useHistory();
+
 	return (
 		<Container>
 			<Voltar onClick={() => history.goBack()} />
+			{formaPagamento.nome}
 			<h2>Carrinho</h2>
 			{carrinho.map(produto => (
 				<Produto {...produto} key={produto.id} />
