35a374758c53f024bae95340fe883d851a7b7a0d
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-31 21:10:29 -0300

    Implementada funcionalidade de adicionar... um item no carrinho introduzindo-o no context Carrinho

--- Alura/React/context_api--feira/src/components/Produto/index.js
+++ Alura/React/context_api--feira/src/components/Produto/index.js
@@ -7,6 +7,27 @@ import { CarrinhoContext } from "common/context/Carrinho";
 
 function Produto({ nome, foto, id, valor, unidade }) {
 	const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+	function adicionarProduto(novoProduto) {
+		const temOProduto = carrinho.some(
+			itemDoCarrinho => itemDoCarrinho.id === novoProduto.id
+		);
+		if (!temOProduto) {
+			novoProduto.quantidade = 1;
+			return setCarrinho(carrinhoAnterior => [
+				...carrinhoAnterior,
+				novoProduto,
+			]);
+		}
+		setCarrinho(carrinhoAnterior =>
+			carrinhoAnterior.map(itemDoCarrinho => {
+				if (itemDoCarrinho.id === novoProduto.id)
+					itemDoCarrinho.quantidade += 1;
+				return itemDoCarrinho;
+			})
+		);
+	}
+
 	return (
 		<Container>
 			<div>
@@ -19,7 +40,7 @@ function Produto({ nome, foto, id, valor, unidade }) {
 				<IconButton color="secondary">
 					<RemoveIcon />
 				</IconButton>
-				<IconButton>
+				<IconButton onClick={() => adicionarProduto({ nome, foto, id, valor })}>
 					<AddIcon />
 				</IconButton>
 			</div>
