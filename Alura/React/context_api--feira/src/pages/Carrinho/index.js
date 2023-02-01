60843c2520ad940a259f57d92f814bee70d9e858
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-01 14:42:35 -0300

    Renderizando o saldo total na pagina

--- Alura/React/context_api--feira/src/pages/Carrinho/index.js
+++ Alura/React/context_api--feira/src/pages/Carrinho/index.js
@@ -8,8 +8,9 @@ import {
 import MuiAlert from "@material-ui/lab/Alert";
 import { useCarrinhoContext } from "common/context/Carrinho";
 import { usePagamentoContext } from "common/context/Pagamento";
+import { UsuarioContext } from "common/context/Usuario";
 import Produto from "components/Produto";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import {
 	Container,
@@ -21,10 +22,13 @@ import {
 function Carrinho() {
 	const [openSnackbar, setOpenSnackbar] = useState(false);
 	const { carrinho, valorTotalCarrinho } = useCarrinhoContext();
+	const { saldo = 0 } = useContext(UsuarioContext);
+
 	const { tiposPagamento, formaPagamento, mudarFormaPagamento } =
 		usePagamentoContext();
 
 	const history = useHistory();
+	const total = saldo - valorTotalCarrinho;
 
 	return (
 		<Container>
@@ -53,17 +57,18 @@ function Carrinho() {
 				</div>
 				<div>
 					<h2> Saldo: </h2>
-					<span> R$ </span>
+					<span> R$ {Number(saldo).toFixed(2)}</span>
 				</div>
 				<div>
 					<h2> Saldo Total: </h2>
-					<span> R$ </span>
+					<span> R$ {total.toFixed(2)}</span>
 				</div>
 			</TotalContainer>
 			<Button
 				onClick={() => {
 					setOpenSnackbar(true);
 				}}
+				disabled={total < 0}
 				color="primary"
 				variant="contained"
 			>
