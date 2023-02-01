1389dd91b8735354a9e4adcb024a7995ebdb50c2
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-01 13:32:06 -0300

    Mudando forma de pagamento

--- Alura/React/context_api--feira/src/pages/Carrinho/index.js
+++ Alura/React/context_api--feira/src/pages/Carrinho/index.js
@@ -1,4 +1,10 @@
-import { Button, Snackbar, InputLabel } from "@material-ui/core";
+import {
+	Button,
+	Snackbar,
+	InputLabel,
+	Select,
+	MenuItem,
+} from "@material-ui/core";
 import MuiAlert from "@material-ui/lab/Alert";
 import { useCarrinhoContext } from "common/context/Carrinho";
 import { PagamentoContext } from "common/context/Pagamento";
@@ -15,20 +21,30 @@ import {
 function Carrinho() {
 	const [openSnackbar, setOpenSnackbar] = useState(false);
 	const { carrinho } = useCarrinhoContext();
-	const { formaPagamento } = useContext(PagamentoContext);
+	const { tiposPagamento, formaPagamento, setFormaPagamento } =
+		useContext(PagamentoContext);
 
 	const history = useHistory();
 
 	return (
 		<Container>
 			<Voltar onClick={() => history.goBack()} />
-			{formaPagamento.nome}
 			<h2>Carrinho</h2>
 			{carrinho.map(produto => (
 				<Produto {...produto} key={produto.id} />
 			))}
 			<PagamentoContainer>
 				<InputLabel> Forma de Pagamento </InputLabel>
+				<Select
+					value={formaPagamento.id}
+					onChange={event => setFormaPagamento(event.target.value)}
+				>
+					{tiposPagamento.map(pagamento => (
+						<MenuItem value={pagamento.id} key={pagamento.id}>
+							{pagamento.nome}
+						</MenuItem>
+					))}
+				</Select>
 			</PagamentoContainer>
 			<TotalContainer>
 				<div>
