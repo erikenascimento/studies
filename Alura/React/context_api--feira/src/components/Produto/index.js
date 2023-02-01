06a898063ecf347e8a5a0e9030811544dc155131
erikenascimento <nascimento.lf.erike@gmail.com>
2023-01-31 23:34:32 -0300

    Implementando a funcionalidade de... remover produto e refatorando o código criando a função  mudarQuantidade pra ajudar na hora de adicionar ou remover produtos

--- Alura/React/context_api--feira/src/components/Produto/index.js
+++ Alura/React/context_api--feira/src/components/Produto/index.js
@@ -6,7 +6,7 @@ import RemoveIcon from "@material-ui/icons/Remove";
 import { useCarrinhoContext } from "common/context/Carrinho";
 
 function Produto({ nome, foto, id, valor, unidade }) {
-	const { carrinho, adicionarProduto } = useCarrinhoContext();
+	const { carrinho, adicionarProduto, removerProduto } = useCarrinhoContext();
 	const produtoNoCarrinho = carrinho.find(
 		itemDoCarrinho => itemDoCarrinho.id === id
 	);
@@ -20,11 +20,14 @@ function Produto({ nome, foto, id, valor, unidade }) {
 				</p>
 			</div>
 			<div>
-				<IconButton color="secondary">
+				<IconButton color="secondary" onClick={() => removerProduto(id)}>
 					<RemoveIcon />
 				</IconButton>
 				{produtoNoCarrinho?.quantidade || 0}
-				<IconButton onClick={() => adicionarProduto({ nome, foto, id, valor })}>
+				<IconButton
+					color="primary"
+					onClick={() => adicionarProduto({ nome, foto, id, valor })}
+				>
 					<AddIcon />
 				</IconButton>
 			</div>
