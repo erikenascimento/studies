93406b9540173cec1c74db2f9a3b2400cf97bef7
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-01 00:08:21 -0300

    Contando a quantidade de produtos... adicionados ao carrinho e mostrando no icone usando badgeContent pelo material-ui

--- Alura/React/context_api--feira/src/pages/Feira/NavBar/index.js
+++ Alura/React/context_api--feira/src/pages/Feira/NavBar/index.js
@@ -1,20 +1,20 @@
-import { Nav } from './styles';
-import { ReactComponent as Logo } from 'assets/logo.svg';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
+import { Nav } from "./styles";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import { useCarrinhoContext } from "common/context/Carrinho";
 
 export default function NavBar() {
-  return (
-    <Nav>
-      <Logo />
-      <IconButton>
-        <Badge
-          color="primary"
-        >
-          <ShoppingCartIcon />
-        </Badge>
-      </IconButton>
-    </Nav>
-  )
+	const { quantidadeProdutos } = useCarrinhoContext();
+	return (
+		<Nav>
+			<Logo />
+			<IconButton disabled={quantidadeProdutos === 0}>
+				<Badge color="primary" badgeContent={quantidadeProdutos}>
+					<ShoppingCartIcon />
+				</Badge>
+			</IconButton>
+		</Nav>
+	);
 }
