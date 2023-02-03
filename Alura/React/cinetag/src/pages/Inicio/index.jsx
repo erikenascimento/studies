f6d8fa7cc185ae1e3afd4052ea666947306e3e24
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-03 14:45:18 -0300

    Finalizado componente do rodape

--- Alura/React/cinetag/src/pages/Inicio/index.jsx
+++ Alura/React/cinetag/src/pages/Inicio/index.jsx
@@ -1,6 +1,12 @@
 import Cabecalho from "components/Cabecalho";
+import Rodape from "components/Rodape";
 import React from "react";
 
 export default function Inicio() {
-	return <Cabecalho />;
+	return (
+		<>
+			<Cabecalho />
+			<Rodape />
+		</>
+	);
 }
