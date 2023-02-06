60080c18e0e128c6da34010eba1c4ab9710887e6
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-06 11:05:08 -0300

    Implementando pagina de favoritos.

--- Alura/React/cinetag/src/pages/Favoritos/index.jsx
+++ Alura/React/cinetag/src/pages/Favoritos/index.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import styles from "./Favoritos.module.scss";
+import Banner from "components/Banner";
+import Card from "components/Card";
+import Titulo from "components/Titulo";
 
 export default function Favoritos() {
-	return <div>Favoritos</div>;
+	return (
+		<>
+			<Banner imagem="favoritos" />
+			<Titulo>
+				<h1>Meus Favoritos</h1>
+			</Titulo>
+			<section className={styles.container}>
+				<Card titulo="Erik" capa="https://github.com/erikenascimento.png" />
+			</section>
+		</>
+	);
 }
