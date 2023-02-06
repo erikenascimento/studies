ae237170d3b019f31f8a0da4fcacd49c90928f93
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-06 11:25:12 -0300

    Criando um hook personalizado com o context... do favorito

--- Alura/React/cinetag/src/pages/Favoritos/index.jsx
+++ Alura/React/cinetag/src/pages/Favoritos/index.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./Favoritos.module.scss";
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
+import { FavoritosContext } from "contexts";
 
 export default function Favoritos() {
 	return (
@@ -16,4 +17,27 @@ export default function Favoritos() {
 			</section>
 		</>
 	);
+}
+
+export function useFavoritoContext() {
+	const { favorito, setFavorito } = useContext(FavoritosContext);
+
+	function adicionarFavorito(novoFavorito) {
+		const favoritoRepetido = favorito.some(item => item.id === novoFavorito.id);
+
+		let novaLista = [...favorito];
+
+		if (!favoritoRepetido) {
+			novaLista.push(novoFavorito);
+			return setFavorito;
+		}
+
+		novaLista.splice(novaLista.indexOf(novoFavorito), 1);
+		return setFavorito(novaLista);
+	}
+
+	return {
+		favorito,
+		adicionarFavorito,
+	};
 }
