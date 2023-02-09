a2614f4120a44b313378d93fb7f4dccec666b502
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-09 10:48:03 -0300

    extendendo a paleta de cores

--- Alura/React/alura_newsletter/src/components/Cabecalho/cabecalho.jsx
+++ Alura/React/alura_newsletter/src/components/Cabecalho/cabecalho.jsx
@@ -2,7 +2,7 @@ import ToggleTheme from "../ToggleTheme/toggleTheme";
 
 export default function Cabecalho() {
 	return (
-		<div className="flex h-20 bg-zinc-700 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+		<div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
 			<span className="text-zinc-100">Ay, Lmao</span>
 			<h1 className="text-zinc-200 text-xl">The aliens are pleased!</h1>
 			<ToggleTheme />
