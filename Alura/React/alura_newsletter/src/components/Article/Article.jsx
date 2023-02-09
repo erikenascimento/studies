d11045357c0f959ce566cb51fafa2bfc3ee34877
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-09 20:14:38 -0300

    Listando e estilizando tags.

--- Alura/React/alura_newsletter/src/components/Article/Article.jsx
+++ Alura/React/alura_newsletter/src/components/Article/Article.jsx
@@ -1,9 +1,19 @@
 export default function Article({ title, text, tags, image, alt }) {
 	return (
-		<div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center">
+		<div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center gap-2">
 			<h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">
 				{title}
 			</h3>
+			<div className="w-full flex justify-end gap-2 pr-5 hidden sm:flex">
+				{tags.map(tag => (
+					<span
+						key={tag}
+						className="bg-alura-100 dark:bg-dark-100 px-3 py-1 rounded-full text-gray-200 dark:text-gray-400 text-xs uppercase font-bold"
+					>
+						{tag}
+					</span>
+				))}
+			</div>
 			<div className="grid gap-1">
 				{text.map((content, index) => (
 					<p key={index} className="text-alura-200 dark:text-gray-300">
