50712b0da2766cc3c97da40b6b343d6387c5d545
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-09 19:33:27 -0300

    Estilizando a lista com Tailwind

---
+++ Alura/React/alura_newsletter/src/components/Article/Article.jsx
@@ -0,0 +1,16 @@
+export default function Article({ title, text, tags, image, alt }) {
+	return (
+		<div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center">
+			<h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">
+				{title}
+			</h3>
+			<div className="grid gap-1">
+				{text.map((content, index) => (
+					<p key={index} className="text-alura-200 dark:text-gray-300">
+						{content}
+					</p>
+				))}
+			</div>
+		</div>
+	);
+}
--- Alura/React/alura_newsletter/src/components/ArticleList/ArticleList.jsx
+++ Alura/React/alura_newsletter/src/components/ArticleList/ArticleList.jsx
@@ -1,10 +1,11 @@
+import Article from "../Article/Article";
 import data from "/articles.json";
 
 export default function ArticleList() {
 	return (
-		<div className="mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3 ">
-			{data.map(props => (
-				<p>{props.title}</p>
+		<div className="mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1000px]">
+			{data.map((props, index) => (
+				<Article key={index} {...props} />
 			))}
 		</div>
 	);
