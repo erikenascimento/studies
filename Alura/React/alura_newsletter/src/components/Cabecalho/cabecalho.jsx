916e82986ef1fd27e21b6cd693bb4f9804390729
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-09 10:28:18 -0300

    Utilizando extensão heroicons

--- Alura/React/alura_newsletter/src/components/Cabecalho/cabecalho.jsx
+++ Alura/React/alura_newsletter/src/components/Cabecalho/cabecalho.jsx
@@ -1,9 +1,11 @@
+import { MoonIcon } from "@heroicons/react/24/solid";
+
 export default function Cabecalho() {
 	return (
 		<div className="flex h-20 bg-zinc-700 justify-between items-center px-5 sm:rounded-xl sm:m-5">
 			<span className="text-zinc-100">Ay, Lmao</span>
 			<h1 className="text-zinc-200 text-xl">The aliens are pleased!</h1>
-			<input className="hidden sm:block" type="checkbox" name="" id="" />
+			<MoonIcon className="hidden sm:block h-8 text-zinc-100" />
 		</div>
 	);
 }
