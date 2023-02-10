8a8c3dce8c64fd4a79073150b1163cf7195fd830
erikenascimento <nascimento.lf.erike@gmail.com>
2023-02-09 21:34:57 -0300

    Personalizando mensagem de usuario

--- Alura/React/alura_newsletter/src/App.jsx
+++ Alura/React/alura_newsletter/src/App.jsx
@@ -1,11 +1,15 @@
-import ArticleList from "./components/ArticleList/ArticleList";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
+import { useState } from "react";
+import ArticleList from "./components/ArticleList/ArticleList";
+import Form from "./components/Form/Form";
 
 export default function App() {
+	const [user, setUser] = useState();
+	const hasUser = Boolean(user);
 	return (
 		<>
-			<Cabecalho />;
-			<ArticleList />
+			<Cabecalho user={user} />;{hasUser && <ArticleList />}
+			{hasUser || <Form onSubmit={setUser} />}
 		</>
 	);
 }
--- Alura/React/alura_newsletter/src/components/Cabecalho/Cabecalho.jsx
+++ Alura/React/alura_newsletter/src/components/Cabecalho/Cabecalho.jsx
@@ -1,9 +1,9 @@
 import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
-export default function Cabecalho() {
+export default function Cabecalho({ user }) {
 	return (
 		<div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
-			<span className="text-zinc-100">Ay, Lmao</span>
+			<span className="text-zinc-100">Olá, {user?.name || "Usuário"}</span>
 			<h1 className="text-zinc-200 text-xl">The aliens are pleased!</h1>
 			<ToggleTheme />
 		</div>
