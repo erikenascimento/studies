4f8685c1b2c312cd772312aeefe7d5645ec151c0
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-15 15:15:59 -0300

    Utilizando programação defensiva para prevenir... usar o setdate para conseguir alterar o valor da data no script.

--- Alura/Typescript/typescript_parte1/app/models/negociacao.ts
+++ Alura/Typescript/typescript_parte1/app/models/negociacao.ts
@@ -1,5 +1,11 @@
 export class Negociacao {
-	constructor(public readonly data: Date, public readonly quantidade: number, public readonly valor: number) {
+	constructor(private _data: Date, public readonly quantidade: number, public readonly valor: number) {
+	}
+
+
+	get data(): Date {
+		const data = new Date(this._data.getTime())
+		return data
 	}
 
 	get volume(): number {
