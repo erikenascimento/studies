596854311fb3f80d7473f4d6f027cbfce1207291
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-15 15:02:36 -0300

    Outra facilitação sintática dessa vez com arrays

--- Alura/Typescript/typescript_parte1/app/models/negociacoes.ts
+++ Alura/Typescript/typescript_parte1/app/models/negociacoes.ts
@@ -1,13 +1,17 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    //private negociacoes: Array<Negociacao> = [];
+    // abaixo temos um "açucar sintático" da opção acima
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao)
     }
 
-    lista(): ReadonlyArray<Negociacao> {
+    // lista(): ReadonlyArray<Negociacao> {
+    // outro "açucar"
+    lista(): readonly Negociacao[] {
         return [...this.negociacoes]
     }
 }
