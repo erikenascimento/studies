9d49d4b2ad5b1712b0e0e77c1eefd45af6d14d16
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-16 10:54:50 -0300

    Corrigindo um erro de sintaxe na interface do... ListaSuspensa quando utilizada por outro componente.

--- Alura/React/Organo_typescript/src/componentes/ListaSuspensa/index.tsx
+++ Alura/React/Organo_typescript/src/componentes/ListaSuspensa/index.tsx
@@ -3,7 +3,7 @@ import './ListaSuspensa.css'
 interface ListaSuspensaProps {
     label: string
     aoAlterado: (valor: string) => void
-    required: boolean
+    obrigatorio: boolean
     valor: string
     itens: string[]
 }
@@ -12,7 +12,7 @@ const ListaSuspensa = (props: ListaSuspensaProps) => {
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.valor}>
                 <option value=""></option>
                 {props.itens.map(item => {
                     return <option key={item}>{item}</option>
