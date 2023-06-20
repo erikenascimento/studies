25d017b429fcd4bcb4fbd0ddb9dcbe25168cd3ce
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-20 12:54:36 -0300

    Criando o componente Card usando styled... components com Emotion

--- Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/App.js
+++ Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { GlobalStyles } from "./components/GlobalStyles/Styles";
+import { Card } from "./components/Card/Card";
 
 function App() {
 	return (
 		<div className="App">
 			<GlobalStyles />
-			<h1>Freelando</h1>
+			<Card>
+				<h1>Freelando</h1>
+			</Card>
 		</div>
 	);
 }
