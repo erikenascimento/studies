36798e368c52630b4ad45e15fbe230901074c7ad
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-20 12:31:46 -0300

    Utilizando estilos globais para aplicar a fonte

--- Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/App.js
+++ Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { GlobalStyles } from "./components/GlobalStyles/Styles";
 
 function App() {
 	return (
 		<div className="App">
+			<GlobalStyles />
 			<h1>Freelando</h1>
 		</div>
 	);
