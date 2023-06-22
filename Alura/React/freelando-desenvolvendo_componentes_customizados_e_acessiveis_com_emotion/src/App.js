a7ec47768e2aeff592b44b283ac8a4342b137bf1
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-22 15:43:53 -0300

    Extendendo o formulario com outros campos

--- Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/App.js
+++ Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/App.js
@@ -6,7 +6,7 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Typography } from "./components/Typography/Typography";
 import { InputText } from "./components/InputText/InputText";
 import { Footer } from "./components/Footer/Footer";
-import { Container } from "react-grid-system";
+import { Col, Container, Row } from "react-grid-system";
 
 function App() {
 	return (
@@ -14,17 +14,46 @@ function App() {
 			<GlobalStyles />
 			<Navbar />
 			<Container>
-				<Card>
-					<Typography variant="h1" component="h1">
-						Crie seu cadastro.
-					</Typography>
-					<Typography variant="body" component="body">
-						Crie seu perfil gratuitamente para começar a trabalhar com os
-						melhores freelancers. Em seguida, você poderá dar mais detalhes
-						sobre suas demandas e sobre sua forma de trabalho.
-					</Typography>
-					<InputText label="Nome Completo" />
-				</Card>
+				<Row justify="center">
+					<Col lg={6} md={8} sm={12}>
+						<Card>
+							<Typography variant="h1" component="h1">
+								Crie seu cadastro.
+							</Typography>
+							<Typography variant="body" component="body">
+								Crie seu perfil gratuitamente para começar a trabalhar com os
+								melhores freelancers. Em seguida, você poderá dar mais detalhes
+								sobre suas demandas e sobre sua forma de trabalho.
+							</Typography>
+							<Row>
+								<Col>
+									<InputText label="Nome Completo" />
+								</Col>
+							</Row>
+							<Row>
+								<Col lg={4} md={4} sm={4}>
+									<InputText label="Estado" />
+								</Col>
+								<Col lg={8} md={8} sm={8}>
+									<InputText label="Cidade" />
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+									<InputText label="E-mail" />
+								</Col>
+							</Row>
+							<Row>
+								<Col lg={6} md={6} sm={6}>
+									<InputText label="Estado" />
+								</Col>
+								<Col lg={6} md={6} sm={6}>
+									<InputText label="Cidade" />
+								</Col>
+							</Row>
+						</Card>
+					</Col>
+				</Row>
 			</Container>
 			<Footer />
 		</ThemeProviderComp>
