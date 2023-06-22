85dbac791ed4db2172502f9ea130082708ab92ae
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-22 15:02:52 -0300

    INstalando react-grid-system pelo modulo Container

--- Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/App.js
+++ Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/App.js
@@ -6,23 +6,26 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Typography } from "./components/Typography/Typography";
 import { InputText } from "./components/InputText/InputText";
 import { Footer } from "./components/Footer/Footer";
+import { Container } from "react-grid-system";
 
 function App() {
 	return (
 		<ThemeProviderComp className="App">
 			<GlobalStyles />
 			<Navbar />
-			<Card>
-				<Typography variant="h1" component="h1">
-					Crie seu cadastro.
-				</Typography>
-				<Typography variant="body" component="body">
-					Crie seu perfil gratuitamente para começar a trabalhar com os melhores
-					freelancers. Em seguida, você poderá dar mais detalhes sobre suas
-					demandas e sobre sua forma de trabalho.
-				</Typography>
-				<InputText label="Nome Completo" />
-			</Card>
+			<Container>
+				<Card>
+					<Typography variant="h1" component="h1">
+						Crie seu cadastro.
+					</Typography>
+					<Typography variant="body" component="body">
+						Crie seu perfil gratuitamente para começar a trabalhar com os
+						melhores freelancers. Em seguida, você poderá dar mais detalhes
+						sobre suas demandas e sobre sua forma de trabalho.
+					</Typography>
+					<InputText label="Nome Completo" />
+				</Card>
+			</Container>
 			<Footer />
 		</ThemeProviderComp>
 	);
