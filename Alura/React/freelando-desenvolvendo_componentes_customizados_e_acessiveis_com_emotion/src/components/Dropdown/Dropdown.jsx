b203e156c8dc29f26e29de7d84d92f9aa9dc5978
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-22 17:16:35 -0300

    Estilizando o dropdown

--- Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/components/Dropdown/Dropdown.jsx
+++ Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/components/Dropdown/Dropdown.jsx
@@ -1,13 +1,58 @@
+import styled from "@emotion/styled";
 import { useState } from "react";
 
-export const Dropdown = ({ label }) => {
+const StyledLabel = styled.label`
+	box-sizing: border-box;
+	display: block;
+	position: relative;
+
+	width: 100%;
+
+	font-weight: 400;
+	font-size: 20px;
+	line-height: 24px;
+`;
+
+const StyledButton = styled.button`
+	box-sizing: border-box;
+	height: 40px;
+	width: 100%;
+
+	align-items: center;
+	display: flex;
+	justify-content: space-between;
+
+	font-size: 18px;
+	font-weight: 400;
+
+	border: 1px solid ${props => props.theme.colors.neutral.a};
+	&:focus {
+		border-color: ${props => props.theme.colors.focus};
+	}
+	border-radius: 18px;
+	border-bottom-left-radius: ${props => (props.isOpen ? "0" : "18px")};
+	border-bottom-right-radius: ${props => (props.isOpen ? "0" : "18px")};
+	outline: none;
+
+	background: ${props => props.theme.colors.white};
+	cursor: pointer;
+
+	margin-top: ${props => props.theme.spacing.xs};
+	padding: ${props => props.theme.spacing.s};
+`;
+
+export const Dropdown = ({ label, options }) => {
 	const [isOpen, toggleVisibility] = useState(false);
+
 	return (
-		<label>
+		<StyledLabel>
 			{label}
-			<button onClick={() => toggleVisibility(!isOpen)}>
-				Selecione<span>{isOpen ? "▲" : "▼"}</span>
-			</button>
-		</label>
+			<StyledButton isOpen={isOpen} onClick={() => toggleVisibility(!isOpen)}>
+				<div>Selecione</div>
+				<div>
+					<span>{isOpen ? "▲" : "▼"}</span>
+				</div>
+			</StyledButton>
+		</StyledLabel>
 	);
 };
