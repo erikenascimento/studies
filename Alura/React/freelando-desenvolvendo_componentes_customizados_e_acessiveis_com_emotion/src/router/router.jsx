093c4a7c6943c9ad3fde5fffebaa7c92e665fe33
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-27 14:18:39 -0300

    Copiano o projeto do repositorio do instrutor... muitos bugs devido à incompatibilidade do node.

--- Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/router/router.jsx
+++ Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/router/router.jsx
@@ -1,9 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom"
+import PaginaInicial from "../paginas/PaginaInicial"
 
 export const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <h1>Freelando</h1>,
-		children: [],
-	},
+    {
+        path: "/",
+        element: <PaginaInicial />,
+        children: [],
+    },
 ]);
