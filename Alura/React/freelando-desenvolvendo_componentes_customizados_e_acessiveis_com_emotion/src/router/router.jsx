49d32b430322ddd7a33cd80b161d36112f1cb404
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-27 17:14:44 -0300

    Removendo arquivos que ficaram do ultimo... projeto e inicializando o router

--- Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/router/router.jsx
+++ Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/router/router.jsx
@@ -1,10 +1,29 @@
-import { createBrowserRouter } from "react-router-dom"
-import PaginaInicial from "../paginas/PaginaInicial"
+import { createBrowserRouter } from "react-router-dom";
+// import PaginaInicial from "../paginas/PaginaInicial";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <PaginaInicial />,
-        children: [],
-    },
+	{
+		path: "/",
+		// element: <PaginaInicial />,
+		children: [
+			{
+				path: "cadastro",
+				element: <h1>Seleção de tipo de cliente</h1>,
+				children: [
+					{
+						path: "cliente",
+						element: <h1>Interesses</h1>,
+					},
+					{
+						path: "dados-pessoais",
+						element: <h1>Dados pessoais</h1>,
+					},
+					{
+						path: "concluido",
+						element: <h1>Concluído</h1>,
+					},
+				],
+			},
+		],
+	},
 ]);
