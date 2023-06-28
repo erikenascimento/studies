6a3325ec1b792a3cb8b93e4cd47146c90c41c9a4
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-28 11:03:36 -0300

    avançando com a página de interesses

---
+++ Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/paginas/cadastro/Interesses.jsx
@@ -0,0 +1,64 @@
+import { Tipografia } from "../../componentes/Tipografia/Tipografia";
+import GrupoRadio from "../../componentes/Radio/GrupoRadio";
+import { useState } from "react";
+import { Col, Row } from "react-grid-system";
+import { Botao } from "../../componentes/Botao/Botao";
+
+const opcoes = [
+	{
+		valor: 1,
+		label: "TI e Programação",
+	},
+	{
+		valor: 2,
+		label: "Design e Multimidia",
+	},
+	{
+		valor: 3,
+		label: "Revisão",
+	},
+	{
+		valor: 4,
+		label: "Tradução",
+	},
+	{
+		valor: 5,
+		label: "Transcrição",
+	},
+	{
+		valor: 6,
+		label: "Marketing",
+	},
+];
+
+const Interesses = () => {
+	const [opcao, setOpcao] = useState("");
+
+	return (
+		<div style={{ textAlign: "center" }}>
+			<Tipografia variante="h1" componente="h1">
+				Crie seu cadastro
+			</Tipografia>
+			<Tipografia variante="h3" componente="h2">
+				Qual a área de interesse?
+			</Tipografia>
+			<GrupoRadio
+				opcoes={opcoes}
+				valor={opcao}
+				onChange={setOpcao}
+			></GrupoRadio>
+			<Row>
+				<Col lg={6} md={6} sm={6}>
+					<Botao variante="secundaria">Anterior</Botao>
+				</Col>
+				<Col lg={6} md={6} sm={6}>
+					<div style={{ textAlign: "right" }}>
+						<Botao>Próxima</Botao>
+					</div>
+				</Col>
+			</Row>
+		</div>
+	);
+};
+
+export default Interesses;
