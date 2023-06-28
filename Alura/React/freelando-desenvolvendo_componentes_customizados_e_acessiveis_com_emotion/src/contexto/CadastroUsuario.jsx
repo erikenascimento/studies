631247fc946ac84a082a80c31abaadca8fe85611
erikenascimento <nascimento.lf.erike@gmail.com>
2023-06-28 14:04:13 -0300

    Criando o provider

--- Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/contexto/CadastroUsuario.jsx
+++ Alura/React/freelando-desenvolvendo_componentes_customizados_e_acessiveis_com_emotion/src/contexto/CadastroUsuario.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const usuarioInicial = {
 	perfil: "",
@@ -18,8 +18,98 @@ export const CadastroUsuarioContext = createContext({
 	setNomeCompleto: () => null,
 	setUf: () => null,
 	setCidade: () => null,
-	setEstado: () => null,
 	setEmail: () => null,
 	setSenha: () => null,
 	setSenhaConfirmada: () => null,
 });
+
+export const useCadastroUsuarioContext = () => {
+	return useState(CadastroUsuarioContext);
+};
+
+export const CadastroUsuarioProvider = ({ children }) => {
+	const [usuario, setUsuario] = useState(usuarioInicial);
+
+	const setPerfil = perfil => {
+		setUsuario(estadoAnterior => {
+			return {
+				...estadoAnterior,
+				setPerfil,
+			};
+		});
+	};
+	const setInteresse = interesse => {
+		setUsuario(estadoAnterior => {
+			return {
+				...estadoAnterior,
+				setPerfil,
+			};
+		});
+	};
+	const setNomeCompleto = nomeCompleto => {
+		setUsuario(estadoAnterior => {
+			return {
+				...estadoAnterior,
+				setPerfil,
+			};
+		});
+	};
+	const setUf = uf => {
+		setUsuario(estadoAnterior => {
+			return {
+				...estadoAnterior,
+				setPerfil,
+			};
+		});
+	};
+	const setCidade = cidade => {
+		setUsuario(estadoAnterior => {
+			return {
+				...estadoAnterior,
+				setPerfil,
+			};
+		});
+	};
+	const setEmail = email => {
+		setUsuario(estadoAnterior => {
+			return {
+				...estadoAnterior,
+				setPerfil,
+			};
+		});
+	};
+	const setSenha = senha => {
+		setUsuario(estadoAnterior => {
+			return {
+				...estadoAnterior,
+				setPerfil,
+			};
+		});
+	};
+	const setSenhaConfirmada = senhaConfirmada => {
+		setUsuario(estadoAnterior => {
+			return {
+				...estadoAnterior,
+				setPerfil,
+			};
+		});
+	};
+
+	const contexto = {
+		usuario,
+		setPerfil,
+		setInteresse,
+		setNomeCompleto,
+		setUf,
+		setCidade,
+		setEmail,
+		setSenha,
+		setSenhaConfirmada,
+	};
+
+	return (
+		<CadastroUsuarioContext.Provider value={contexto}>
+			{children}
+		</CadastroUsuarioContext.Provider>
+	);
+};
