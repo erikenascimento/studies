7561d3d0f617676830ce7baa9d811f50d1de29bc
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-05 11:44:37 -0300

    Isolando a logica de conversao de entrada

--- Alura/Typescript/typescript_parte1/app/controllers/negociacao-controllers.ts
+++ Alura/Typescript/typescript_parte1/app/controllers/negociacao-controllers.ts
@@ -22,7 +22,12 @@ export class NegociacaoController {
     }
 
     public adiciona(): void {
-        const negociacao = this.criaNegociacao()
+        const negociacaoTemp = new Negociacao(null, 0, 0)
+        const negociacao = negociacaoTemp.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        )
         if(!this.serDiaUtil(negociacao.data)) {
             this.mensagemView.update('Apenas negociações em dias úteis são aceitas')
             return;
@@ -34,14 +39,6 @@ export class NegociacaoController {
 
     private serDiaUtil(data: Date): boolean {
         return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO
-    }
-
-    private criaNegociacao(): Negociacao {
-        const exp = /-/g
-        const date = new Date(this.inputData.value.replace(exp, ','))
-        const quantidade = parseInt(this.inputQuantidade.value)
-        const valor = parseFloat(this.inputValor.value)
-        return new Negociacao(date, quantidade, valor)
     }
 
     private limparFormulario(): void{
