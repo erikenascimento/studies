236839f5e36a40c97bd8ef64466a93e8a58c0d70
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-07 11:34:41 -0300

    criando um decorator para auxiliar no queryselector

--- Alura/Typescript/app/src/views/view.ts
+++ Alura/Typescript/app/src/views/view.ts
@@ -14,8 +14,8 @@ export abstract class View<T> {
         }
     }
 
-    @inspect()
-    @logarTempoDeExecucao()
+    // @inspect()
+    // @logarTempoDeExecucao()
     public update(model: T): void {
         let template = this.template(model);
         this.elemento.innerHTML = template;
