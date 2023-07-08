b8b31d6a28fd621a6f3af193e4269ae9b5b0597d
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-08 10:27:50 -0300

    Criando novo componente de lista

--- Alura/React/react-escrevendo_com_typescript/src/App.tsx
+++ Alura/React/react-escrevendo_com_typescript/src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Form from './components/Form';
+import React from "react";
+import Form from "./components/Form";
+import List from "./components/List";
 
 function App() {
-  return (
-    <div className="App">
-      <Form />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Form />
+			<List />
+		</div>
+	);
 }
 
 export default App;
---
+++ Alura/React/react-escrevendo_com_typescript/src/components/List/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function List() {
+	return (
+		<aside>
+			<h2>Daily studies.</h2>
+			<ul>
+				<li>
+					<h3>React</h3>
+					<span>02:00:00</span>
+				</li>
+				<li>
+					<h3>Javascript</h3>
+					<span>01:00:00</span>
+				</li>
+			</ul>
+		</aside>
+	);
+}
