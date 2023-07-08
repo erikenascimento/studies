42bd26c61535df87411a0f053522a12cf6df4286
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-08 10:21:01 -0300

    Criando segundo componente, Form

---
+++ Alura/React/react-escrevendo_com_typescript/src/components/Form/index.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Button from '../Button'
+
+export default class Form extends Component {
+  render() {
+    return (
+      <form>
+        <div>
+            <label htmlFor='task'>
+              Add a new study.
+            </label>
+            <input type="text" 
+            name='task' id='task' 
+            placeholder='What do you want to study?'/>
+        </div>
+        <div>
+            <label htmlFor='time'>
+              Time
+            </label>
+            <input type="time"
+            step='1' 
+            name='time' 
+            id='time' 
+            min='00:00:00'
+            max='01:30:00'
+            required />
+        </div>
+            <Button/>
+      </form>
+    )
+  }
+}
