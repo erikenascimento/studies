c290d8c91efafd79b734603dc84a882f9b72cf96
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-08 17:10:25 -0300

    Utilizando o usestate para adicionar uma tarefa e exibila no console

--- Alura/React/react-escrevendo_com_typescript/src/components/Form/index.tsx
+++ Alura/React/react-escrevendo_com_typescript/src/components/Form/index.tsx
@@ -3,15 +3,28 @@ import Button from "../Button";
 import style from "./Form.module.scss";
 
 export default class Form extends Component {
+	state = {
+		task: "",
+		time: "00:00:00",
+	};
+
+	addTask(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		console.log("state", this.state);
+	}
 	render() {
 		return (
-			<form className={style.newTask}>
+			<form className={style.newTask} onSubmit={this.addTask.bind(this)}>
 				<div className={style.inputContainer}>
 					<label htmlFor="task">Add a new study.</label>
 					<input
 						type="text"
 						name="task"
 						id="task"
+						value={this.state.task}
+						onChange={event =>
+							this.setState({ ...this.state, task: event.target.value })
+						}
 						placeholder="What do you want to study?"
 					/>
 				</div>
@@ -21,6 +34,10 @@ export default class Form extends Component {
 						type="time"
 						step="1"
 						name="time"
+						value={this.state.time}
+						onChange={event =>
+							this.setState({ ...this.state, time: event.target.value })
+						}
 						id="time"
 						min="00:00:00"
 						max="01:30:00"
