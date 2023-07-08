b440106f25c822570079a726dc8a523fe55eba1b
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-08 17:48:23 -0300

    Baixando projeto novamente do repositório... Dei npm audit fix --force e quebrei o projeto fazendo atualizações indevidas que por exemplo quebrou o class component.

--- Alura/React/react-escrevendo_com_typescript/src/components/Button/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from "./Button.module.scss";
-
-export default class Button extends React.Component<any, any> {
-	render() {
-		return <button className={style.button}>{this.props.children}</button>;
-	}
-}
