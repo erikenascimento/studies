3f4130f62bffe6e0fbde623a4b46cc367908f45b
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-12 14:37:17 -0300

    Criando um util para converter o tempo em segundos para ser exibido depois no cronometro

--- Alura/React/alura_studies-react-escrevendo_com_typescript/src/components/Cronometro/index.tsx
+++ Alura/React/alura_studies-react-escrevendo_com_typescript/src/components/Cronometro/index.tsx
@@ -1,17 +1,16 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
-import style from './Cronometro.module.scss';
+import style from "./Cronometro.module.scss";
+import { tempoParaSegundos } from "../../common/utils/date";
 
 export default function Cronometro() {
-  return (
-    <div className={style.cronometro}>
-      <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
-      <div className={style.relogioWrapper}>
-        <Relogio />
-      </div>
-      <Botao>
-        Começar!
-      </Botao>
-    </div>
-  )
+	return (
+		<div className={style.cronometro}>
+			<p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
+			<div className={style.relogioWrapper}>
+				<Relogio />
+			</div>
+			<Botao>Começar!</Botao>
+		</div>
+	);
 }
