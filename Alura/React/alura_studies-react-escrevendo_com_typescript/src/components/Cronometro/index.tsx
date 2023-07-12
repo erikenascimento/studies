b679666da7270fbf2ad02c6df0e2497754763528
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-12 14:40:11 -0300

    mudando o nome de util para um nome melhor

--- Alura/React/alura_studies-react-escrevendo_com_typescript/src/components/Cronometro/index.tsx
+++ Alura/React/alura_studies-react-escrevendo_com_typescript/src/components/Cronometro/index.tsx
@@ -1,7 +1,7 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
-import { tempoParaSegundos } from "../../common/utils/date";
+import { tempoParaSegundos } from "../../common/utils/time";
 
 export default function Cronometro() {
 	return (
