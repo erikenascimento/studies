ffe0a43653553b0f6deecf8e025fc94d60c795b6
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-24 11:53:28 -0300

    Criando os componentes para a filtragem do cardapio

--- Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/pages/Cardapio/index.tsx
+++ Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/pages/Cardapio/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Buscador from "./Buscador";
 import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import Filtros from "./Filtros";
 
 export default function Cardapio() {
 	const [busca, setBusca] = useState("");
@@ -17,6 +18,9 @@ export default function Cardapio() {
 			<section className={styles.cardapio}>
 				<h3 className={styles.cardapio__titulo}>Cardapio</h3>
 				<Buscador busca={busca} setBusca={setBusca} />
+				<div className={styles.cardapio__filtros}>
+					<Filtros />
+				</div>
 			</section>
 		</main>
 	);
