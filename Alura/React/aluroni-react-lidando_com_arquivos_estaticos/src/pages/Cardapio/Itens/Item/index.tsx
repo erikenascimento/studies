91614a6bb2c83ae9b7c81735a399a512244cd151
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-24 13:44:03 -0300

    Atualizando o estilo das tags

--- Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/pages/Cardapio/Itens/Item/index.tsx
+++ Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,5 +1,6 @@
 import styles from "./Item.module.scss";
 import cardapio from "../itens.json";
+import classNames from "classnames";
 
 type Props = (typeof cardapio)[0];
 
@@ -16,7 +17,15 @@ export default function Item(props: Props) {
 					<p>{description}</p>
 				</div>
 				<div className={styles.item__tags}>
-					<div className={styles.item__tipo}>{category.label}</div>
+					<div
+						className={classNames({
+							[styles.item__tipo]: true,
+							[styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]:
+								true,
+						})}
+					>
+						{category.label}
+					</div>
 					<div className={styles.item__porcao}>{size}g</div>
 					<div className={styles.item__qtdpessoas}>
 						Serve {serving} pessoa${serving === 1 ? "" : "s"}
