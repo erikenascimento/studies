1284efde755244e81bd09500b27dd0cd9c059ad7
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-24 13:36:31 -0300

    Populando cada item do cardapio utilizando o json itens

--- Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/pages/Cardapio/Itens/Item/index.tsx
+++ Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,21 +1,27 @@
 import styles from "./Item.module.scss";
+import cardapio from "../itens.json";
 
-export default function Item() {
+type Props = (typeof cardapio)[0];
+
+export default function Item(props: Props) {
+	const { title, description, category, size, serving, price, photo } = props;
 	return (
 		<div className={styles.item}>
 			<div className={styles.item__imagem}>
-				<img src="" alt="imagem" />
+				<img src={photo} alt={title} />
 			</div>
 			<div className={styles.item__descricao}>
 				<div className={styles.item__titulo}>
-					<h2>Título</h2>
-					<p>Descrição</p>
+					<h2>{title}</h2>
+					<p>{description}</p>
 				</div>
 				<div className={styles.item__tags}>
-					<div className={styles.item__tipo}>Tag</div>
-					<div className={styles.item__porcao}>XXXg</div>
-					<div className={styles.item__qtdpessoas}>Serve x pessoas</div>
-					<div className={styles.item__valor}>XX R$</div>
+					<div className={styles.item__tipo}>{category.label}</div>
+					<div className={styles.item__porcao}>{size}g</div>
+					<div className={styles.item__qtdpessoas}>
+						Serve {serving} pessoa${serving === 1 ? "" : "s"}
+					</div>
+					<div className={styles.item__valor}>{price.toFixed(2)} R$</div>
 				</div>
 			</div>
 		</div>
--- Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/pages/Cardapio/Itens/index.tsx
+++ Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/pages/Cardapio/Itens/index.tsx
@@ -6,7 +6,7 @@ export default function Itens() {
 	return (
 		<div className={styles.item}>
 			{cardapio.map(item => (
-				<Item key={item.id} />
+				<Item key={item.id} {...item} />
 			))}
 		</div>
 	);
