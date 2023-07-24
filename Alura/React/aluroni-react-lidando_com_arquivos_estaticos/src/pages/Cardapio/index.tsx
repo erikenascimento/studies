2b49a6630710656f8cb5440343bb14e6ceafb7dc
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-24 11:10:43 -0300

    Criando o componente buscador

--- Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/pages/Cardapio/index.tsx
+++ Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/pages/Cardapio/index.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+import Buscador from "./Buscador";
 import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
 
 export default function Cardapio() {
+	const [busca, setBusca] = useState("");
+
 	return (
 		<main>
 			<nav className={styles.menu}>
@@ -10,6 +14,10 @@ export default function Cardapio() {
 			<header className={styles.header}>
 				<div className={styles.header__text}>A casa do código e da massa</div>
 			</header>
+			<section className={styles.cardapio}>
+				<h3 className={styles.cardapio__titulo}>Cardapio</h3>
+				<Buscador busca={busca} setBusca={setBusca} />
+			</section>
 		</main>
 	);
 }
