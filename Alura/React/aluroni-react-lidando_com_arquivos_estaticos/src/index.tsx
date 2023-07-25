788160391bd71a967ffa0bbb52d70b051fc21c56
erikenascimento <nascimento.lf.erike@gmail.com>
2023-07-25 14:56:16 -0300

    Problema com o eslint ao dar sequencia com o projeto que desenvolvi no módulo 1, baixando o projeto do módulo 2 direto do repositório

--- Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/index.tsx
+++ Alura/React/aluroni-react-lidando_com_arquivos_estaticos/src/index.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Cardapio from "./pages/Cardapio";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import './index.css';
+import Cardapio from './pages/Cardapio';
 
-const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
-);
-root.render(
-	<React.StrictMode>
-		<Cardapio />
-	</React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <Cardapio />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
