e3c950ba77fbccb8921eed4ce87533002f808185
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-14 17:34:25 -0300

    Executando os testes com mochawesome

--- Alura/Outros/QA/alurapic/cypress.config.ts
+++ Alura/Outros/QA/alurapic/cypress.config.ts
@@ -3,7 +3,18 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      {
+        ("reporter");
+        "mochawesome", "reporterOptions";
+        {
+          ("reportDir");
+          "cypress/report/mochawesome-report", "overwrite";
+          true, "html";
+          true, "json";
+          false, "timestamp";
+          ("mmddyyyy_HHMMss");
+        }
+      }
     },
   },
 });
