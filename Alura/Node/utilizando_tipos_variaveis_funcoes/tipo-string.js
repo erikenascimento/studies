7e59b5e19fe0550561b09b04428a9df3561f409d
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-13 21:51:48 -0300

    Aula sobre strings

---
+++ Alura/Node/utilizando_tipos_variaveis_funcoes/tipo-string.js
@@ -0,0 +1,11 @@
+const estudante = `Caroline`;
+const docente = `Ana`;
+const lema = `nosso lema é 'estudar bastante'`;
+const citacao = `Ju diz: "nosso lema é 'estudar bastante!'"`;
+
+console.log("lema:", lema);
+console.log("citacao:", citacao);
+console.log(`a estudante chama ${estudante}`);
+
+const senha = "SenhaSegura123" + estudante.toUpperCase();
+console.log(senha);
