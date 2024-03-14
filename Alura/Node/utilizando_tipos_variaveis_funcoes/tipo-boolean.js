9cea5b5fd5a9204879cbaf563b082591e88e7620
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-13 21:55:13 -0300

    AUla sobre tipos booleanos

---
+++ Alura/Node/utilizando_tipos_variaveis_funcoes/tipo-boolean.js
@@ -0,0 +1,14 @@
+const estudante = "Fernando";
+const estaAprovado = true;
+
+if (estaAprovado) {
+	console.log("parabéns, noas festas");
+} else {
+	console.log("REPROVADO, boas festas");
+}
+
+if (estudante === "Fernando") {
+	console.log(`olá, ${estudante}`);
+} else {
+	console.log("quem é você?");
+}
