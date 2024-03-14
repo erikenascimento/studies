31882988684883f303a6b6a8fa3a251bcb9206f8
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-13 22:04:12 -0300

    finalizando aula sobre tipos null e undefined

---
+++ Alura/Node/utilizando_tipos_variaveis_funcoes/tipo-null-undefined.js
@@ -0,0 +1,10 @@
+let nomeEstudante;
+const professora = "Ana";
+
+console.log("nomeEstudante:", nomeEstudante);
+console.log("tipo nomeEstudante:", typeof nomeEstudante);
+console.log("tipo professora:", typeof professora);
+
+let telefoneEstudante = null;
+
+console.log(typeof telefoneEstudante);
