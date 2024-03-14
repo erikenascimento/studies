939b89faba0472a6df45d4217d8a3284d509b8dd
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-14 20:56:37 -0300

    Criando um dublê para testes

---
+++ Alura/Outros/QA/alurapic/cypress/e2e/login-incorreto.cy.js
@@ -0,0 +1,19 @@
+describe("Página de login", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:4200/#/home");
+
+    cy.intercept("POST", "http://localhost:3000/user/login", {
+      statusCode: 400,
+    }).as("stubPost");
+  });
+
+  it("Verificar mensagens de campos obrigatórios", () => {
+    cy.contains("User name is required!").should("be.visible");
+    cy.contains("Password is required!").should("be.visible");
+  });
+
+  it("Deve falhar mesmo que os campos sejam preenchidos corretamente", () => {
+    cy.login("carowl", "12345678");
+    cy.wait("@stubPost");
+  });
+});
--- Alura/Outros/QA/alurapic/cypress/e2e/login-incorreto.cy.ts
+++
@@ -1,8 +0,0 @@
-describe("Página de login", () => {
-  it("Verificar mensagens de campos obrigatórios", () => {
-    cy.visit("http://localhost:4200/#/home");
-
-    cy.contains("User name is required!").should("be.visible");
-    cy.contains("Password is required!").should("be.visible");
-  });
-});
