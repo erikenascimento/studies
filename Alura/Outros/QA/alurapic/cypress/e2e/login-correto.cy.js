af78b042201b1dd92d8516ca74810c4dadaa4bef
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-14 20:35:41 -0300

    Criando comandos personalizados. Alguns arquivos tiveram que ser renomeados para js, a fim de funcionar como mostrado na aula.

---
+++ Alura/Outros/QA/alurapic/cypress/e2e/login-correto.cy.js
@@ -0,0 +1,8 @@
+describe("Página de login", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:4200/#/home");
+  });
+  it("Preencher os campos do formulário corretamente para efetuar login", () => {
+    cy.login("catarinap", "catarina123");
+  });
+});
