7a8fde257a42dfb91923563ebd18fcab4dcdc4a3
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-18 12:31:08 -0300

    Incluindo comportamentos adicionais com interfaces

---
+++ Alura/Java/screenmatch/src/br/com/alra/screenmatch/calculos/Classificavel.java
@@ -0,0 +1,5 @@
+package br.com.alra.screenmatch.calculos;
+
+public interface Classificavel {
+    int getClassificacao();
+}
---
+++ Alura/Java/screenmatch/src/br/com/alura/screenmatch/modelos/Episodio.java
@@ -0,0 +1,7 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Episodio {
+    private int numero = 1;
+    private String nome = "";
+    private Serie sere;
+}
