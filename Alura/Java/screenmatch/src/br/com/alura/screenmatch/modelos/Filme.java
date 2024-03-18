3eac354a12807282301712b85f1e010299f937eb
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-18 10:24:13 -0300

    Criando getters e setter para os atributos

--- Alura/Java/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ Alura/Java/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -3,16 +3,47 @@ package br.com.alura.screenmatch.modelos;
 import org.w3c.dom.ls.LSOutput;
 
 public class Filme {
-    public String nome = "";
-    int anoDeLancamento;
-    String descricao = "";
-    boolean incluidoNoPlano;
+    private String nome = "";
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
     private double somaDasAvaliacoes = 0.0;
     private int totalDeAvaliacoes = 0;
-    int duracaoEmMinutos;
+    private int duracaoEmMinutos;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
 
     public int getTotalDeAvaliacoes() {
         return totalDeAvaliacoes;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
     }
 
     public void exibeFichaTecnica() {
