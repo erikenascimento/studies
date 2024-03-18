bfdef38199153bd0a8750e3f3ca7dbf049968b0d
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-18 09:14:53 -0300

    Iniciando o modelo de filme

---
+++ Alura/Java/screenmatch/src/Filme.java
@@ -0,0 +1,10 @@
+public class Filme {
+    String nome = "";
+    int anoDeLancamento;
+    String descricao = "";
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int popularidade;
+    int duracaoEmMinutos;
+
+}
