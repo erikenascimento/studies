86e2c0acbcdeda73d47470acc79ce9a7a1582e94
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-18 09:31:44 -0300

    Criando métodos de avaliação para o filme

--- Alura/Java/screenmatch/src/Filme.java
+++ Alura/Java/screenmatch/src/Filme.java
@@ -1,10 +1,25 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Filme {
     String nome = "";
     int anoDeLancamento;
     String descricao = "";
     boolean incluidoNoPlano;
-    double avaliacao;
-    int popularidade;
+    double somaDasAvaliacoes = 0.0;
+    int totalDeAvaliacoes = 0;
     int duracaoEmMinutos;
 
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia() {
+        return somaDasAvaliacoes/totalDeAvaliacoes;
+    }
 }
