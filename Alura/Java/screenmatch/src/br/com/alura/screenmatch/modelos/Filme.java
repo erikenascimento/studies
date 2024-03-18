9f43fefcb547b18bea1a0bce14e37cc8a2ca8639
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-18 10:56:20 -0300

    Criando um modelo titulo para ser herdado por filmes e series

--- Alura/Java/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ Alura/Java/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,62 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-import org.w3c.dom.ls.LSOutput;
+public class Filme extends Titulo{
+    private String diretor = "";
 
-public class Filme {
-    private String nome = "";
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes = 0.0;
-    private int totalDeAvaliacoes = 0;
-    private int duracaoEmMinutos;
-
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public void exibeFichaTecnica() {
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-    }
-
-    public void avalia(double nota) {
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia() {
-        return somaDasAvaliacoes/totalDeAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
