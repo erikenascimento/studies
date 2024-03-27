c389830379a466d19b578864f60a456815d415de
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-27 11:56:16 -0300

    Criando novo component de listaPensamento e trabalhando com rotas através do app-routing

--- Alura/Outros/Angular/memoteca/src/app/app-routing.module.ts
+++ Alura/Outros/Angular/memoteca/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListaPensamentoComponent } from './components/pensamentos/lista-pensamento/lista-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listaPensamento',
+    pathMatch: 'full',
+  },
+  {
+    path: 'criarPensamento',
+    component: CriarPensamentoComponent,
+  },
+  {
+    path: 'listaPensamento',
+    component: ListaPensamentoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ Alura/Outros/Angular/memoteca/src/app/components/pensamentos/lista-pensamento/lista-pensamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-pensamento',
+  templateUrl: './lista-pensamento.component.html',
+  styleUrls: ['./lista-pensamento.component.css']
+})
+export class ListaPensamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
