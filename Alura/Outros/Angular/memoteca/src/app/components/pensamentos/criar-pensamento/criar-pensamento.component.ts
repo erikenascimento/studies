b99d593f77eeb13f3c74ec80e5183b70c77769de
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-03-27 10:14:45 -0300

    Criando um data-binding do elemento pro component

--- Alura/Outros/Angular/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ Alura/Outros/Angular/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -16,4 +16,12 @@ export class CriarPensamentoComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  criarPensamento() {
+    alert('Funciona');
+  }
+
+  cancelarPensamento() {
+    alert('Cancelado!');
+  }
 }
